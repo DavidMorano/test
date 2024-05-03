d203e22fe7b7a48b059cd447365b93f2a01c40e1
David A.D. Morano <david.a.morano@gmail.com>
2024-05-03 03:01:08 -0400

    test |sbrk(2)|
    ok

--- src/testos/main.cc
+++ src/testos/main.cc
@@ -1,10 +1,17 @@
 /* main SUPPOER (testlang */
+/* lang=C++20 */
+
+/* testing operating system (OS) feathres */
+/* version %I% last-modified %G% */
+
+#define	CF_SBRK		0		/* test for depracation of |sbrk(2)| */
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<sys/types.h>
 #include	<sys/param.h>
 #include	<sys/wait.h>
+#include	<unistd.h>		/* |sbrk(2)| */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<iostream>
@@ -43,10 +50,19 @@ int main(int argc,mainv,mainv) {
 	cout << a << eol ;
 	cout << lamb() << eol ;
 
+/* does |waitid(2)| compile (and link) properly? */
+
 	{
 	    idtype_t	t = static_cast<idtype_t>(0) ;
 	    waitid(t,0,nullptr,0) ;
 	}
+
+/* can we call (my beloved) |sbrk(2)| without it being depracated? */
+
+#if	CF_SBRK
+	sbrk(2) ;
+#endif /* CF_SBRK */
+
 
 	if ((ex == EX_OK) && (rs < 0)) ex = EX_DATAERR ;
 	return ex ;
