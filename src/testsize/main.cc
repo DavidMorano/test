116a64b885d52bdd9c6faa9113e7f99b0e5f9a90
David A.D. Morano <david.a.morano@gmail.com>
2024-05-26 19:43:41 -0400

    innocuous

--- src/testsize/main.cc
+++ src/testsize/main.cc
@@ -1,9 +1,13 @@
-/* main (testsize) */
+/* main SUPPORT (testsize) */
 
 
+#include	<envstandards.h>	/* MUST be ordered first to configure */
 #include	<sys/types.h>
-#include	<stdio.h>
-
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdio>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
 #include	<localmisc.h>
 
 
@@ -14,18 +18,25 @@ struct testsize {
 	uint	c : 1 ;
 } ;
 
+struct structshort {
+	short	a ;
+} ;
+
+struct structbits {
+	short	ch ;
+	uint	a:1 ;
+	uint	b:1 ;
+} ;
 
 
-int main()
-{
-
-
-	printf("sizeof testsize=%d\n",sizeof(struct testsize)) ;
-
-	fclose(stdout) ;
-
-	return 0 ;
+int main() {
+	int	sz ;
+	sz = sizeof(struct testsize) ;
+	printf("sizeof testsize=%d\n",sz) ;
+	sz = sizeof(structshort) ;
+	printf("sz=%d\n",sz) ;
+	sz = sizeof(structbits) ;
+	printf("sz=%d\n",sz) ;
 }
-
 
 
