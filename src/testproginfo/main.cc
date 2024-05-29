cf0f4cdd3f3e5e7f7df2f171450c1d5f6af00039
David A.D. Morano <david.a.morano@gmail.com>
2024-05-28 21:36:52 -0400

    test enhancement

--- src/testproginfo/main.cc
+++ src/testproginfo/main.cc
@@ -1,37 +1,32 @@
-/* main */
+/* main SUPPORT */
+/* lang=C++20 */
 
 
+#include	<envstandards.h>	/* MUST be ordered first to configure */
 #include	<sys/types.h>
-#include	<stdlib.h>
-#include	<stdio.h>
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>		/* |geprogname(3c)| */
+#include	<cstdio>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysrets.h>
+#include	<usyscalls.h>
+#include	<localmisc.h>
 
 
 
-int main(argc,argv,envv)
-char	argc ;
-char	*argv[] ;
-char	*envv[] ;
-{
-	int	rs ;
+int main(int argc,mainv argv,mainv envv) {
+	cchar		*cp ;
 
-	char	*cp ;
-
-
-
-	fprintf(stdout,"argname=%s\n",
-		((argc > 0) ? argv[0] : "")) ;
+	printf("argname=%s\n", ((argc > 0) ? argv[0] : "")) ;
 
 	cp = getenv("_") ;
+	printf("prgfname=%s\n", ((cp != nullptr) ? cp : "")) ;
 
-	fprintf(stdout,"prgfname=%s\n",
-		((cp != NULL) ? cp : "")) ;
-
-
-	fclose(stdout) ;
-
-	return 0 ;
+	cp = getprogname() ;
+	printf("progname=%s\n", ((cp != nullptr) ? cp : "")) ;
 }
 /* end subroutine (main) */
-
 
 
