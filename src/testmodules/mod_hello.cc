33971b08d73ba8a41dfd73342e758ca1da07d23a
David A.D. Morano <david.a.morano@gmail.com>
2025-01-24 16:23:28 -0500

    re-org

---
+++ src/testmodules/mod_hello.cc
@@ -0,0 +1,21 @@
+/* hello MODULE */
+
+export module hello ;
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<iostream>
+#include	<iomanip>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+#include	<usyscalls.h>
+#include	<localmisc.h>
+
+export void hello_there ()  noex {
+	cout << "Hello there!" << eol ;
+}
+
