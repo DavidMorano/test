b9e18b3ee34d745d83604a4e36a8a29b0c628fa0
David A.D. Morano <david.a.morano@gmail.com>
2025-02-05 23:54:04 -0500

    variation

---
+++ src/testmisc/testconstc.c
@@ -0,0 +1,59 @@
+/* testconst SUPPORT (testconst) */
+/* encoding=ISO8859-1 */
+/* lang=C20 */
+
+
+/* revision history:
+
+	= 2013-07-11, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2013 David A­D­ Morano.  All rights reserved. */
+
+#include	<envstandards.h>	/* MUST be ordered first to configure */
+#include	<stdio.h>
+#include	<usystem.h>
+#include	<localmisc.h>
+
+
+/* local defines */
+
+
+/* name-spaces */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures (and methods) */
+
+
+/* forward references */
+
+
+/* local variables */
+
+static const char	str[] = "Hello world!\n" ;
+
+const int	pi = 3 ;
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int main(int,mainv,mainv) {
+    	printf(str) ;
+}
+/* end subroutine (main) */
+
+
+/* local subroutines */
+
+
