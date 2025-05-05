86dd3378302496ffc444bd77c59c844dd03464f0
David A.D. Morano <david.a.morano@gmail.com>
2025-05-05 01:41:10 -0400

    transfer

---
+++ src/testlib/libtest.cc
@@ -0,0 +1,101 @@
+/* libtest SUPPORT */
+/* encoding=ISO8859-1 */
+/* lang=C++20 */
+
+/* test library */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-13, David A-D- Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<cstdlib>
+#include	<cstring>
+#include	<utility>
+#include	<dlfcn.h>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+#include	<iostream>
+#include	<localmisc.h>
+
+
+/* local defines */
+
+
+/* imported namespaces */
+
+using namespace std ;
+
+
+/* local namesapces */
+
+extern "C" {
+    typedef void (*voidint_f)(int) noex ;
+}
+
+
+/* external subroutines */
+
+extern "C" {
+    extern void sub() noex ;
+    extern void exit(int) noex ;
+}
+
+extern "C" {
+    static void efunc() noex ;
+}
+
+namespace {
+    struct libobj {
+	libobj() noex {
+	    cchar	*sp = "libtest.ctor\n" ;
+	    cint	sl = strlen(sp) ;
+	    write(1,sp,sl) ;
+	} ;
+	destruct libobj() noex {
+	    cchar	*sp = "libtest.dtor\n" ;
+	    cint	sl = strlen(sp) ;
+	    write(1,sp,sl) ;
+	} ;
+    } ;
+}
+
+static const libobj	obj_data ;
+
+void sub() noex {
+	cchar	*sp = "libtest.sub\n" ;
+	cint	sl = strlen(sp) ;
+	write(1,sp,sl) ;
+	atexit(efunc) ;
+}
+
+void exit(int ex) {
+	cchar	*sp = "libtest.exit\n" ;
+	cint	sl = strlen(sp) ;
+	write(1,sp,sl) ;
+	{
+	    void	*dlp = dlsym(RTLD_NEXT,"exit") ;
+	    voidint_f	ef = (voidint_f) dlp ;
+	    (*ef)(ex) ;
+	}
+	std::unreachable() ;
+}
+
+static void efunc() noex {
+	cchar	*sp = "libtest.efunc\n" ;
+	cint	sl = strlen(sp) ;
+	write(1,sp,sl) ;
+}
+
+
