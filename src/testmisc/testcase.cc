5aaf92de06cf4cd5b8ca6b539426ab5a5075090e
David A.D. Morano <david.a.morano@gmail.com>
2025-07-11 00:04:16 -0400

    update

--- src/testmisc/testcase.cc
+++ src/testmisc/testcase.cc
@@ -4,7 +4,6 @@
 
 
 #include	<envstandards.h>	/* must be ordered first to configure */
-#include	<unistd.h>		/* |sysconf(2)| */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<cstdio>
@@ -15,16 +14,27 @@
 #include	<utypealiases.h>
 #include	<usysdefs.h>
 #include	<usysrets.h>
+#include	<strn.h>		/* |strnsub(3uc)| */
 #include	<localmisc.h>
 
 using std::cout ;			/* variable */
 
 cchar		strmain[] = "Héllo" ;
-cchar		strsub[] = "Hél" ;
+cchar		strsub[] = "HÉl" ;
 
 int main(int,mainv,mainv) {
-    if (int rc ; (rc = strcasecmp(strmain,strsub)) == 0) {
-	cout << "yes" << eol ;
+    	cnullptr	np{} ;
+    if (cc *tp ; (tp = strstr(strmain,strsub)) != np) {
+	cout << "strstr" << eol ;
+    }
+    if (cc *tp ; (tp = strcasestr(strmain,strsub)) != np) {
+	cout << "strcasestr" << eol ;
+    }
+    if (cc *tp ; (tp = strncasesub(strmain,-1,strsub)) != np) {
+	cout << "strncasesub" << eol ;
+    }
+    if (cc *tp ; (tp = strnfoldsub(strmain,-1,strsub)) != np) {
+	cout << "strnfoldsub" << eol ;
     }
 } /* end subroutine (main) */
 
