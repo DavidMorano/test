8c1e683f8b1af82a30016d505b807d4ef5e9c540
David A.D. Morano <david.a.morano@gmail.com>
2025-07-22 01:03:42 -0400

    rename

--- src/testprogname/testprogname_main.cc
+++ src/testprogname/testprogname_main.cc
@@ -5,18 +5,25 @@
 
 #include	<envstandards.h>	/* MUST be ordered first to configure */
 #include	<sys/types.h>
+#include	<sys/sysctl.h>
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>		/* |geprogname(3c)| */
 #include	<cstdio>
 #include	<clanguage.h>
 #include	<utypedefs.h>
 #include	<utypealiases.h>
+#include	<usysdefs.h>
 #include	<usysrets.h>
 #include	<usyscalls.h>
-#include	<localmisc.h>
+#include	<localmisc.h>		/* |MAXPATHLEN| */
 
 
 /* local defines */
+
+
+/* local variables */
+
+cint		maxpath = MAXPATHLEN ;
 
 
 /* exported variables */
@@ -24,7 +31,9 @@
 
 /* exported subroutines */
 
-int main(int argc,mainv argv) {
+int main(int argc,mainv argv,mainv) {
+    	cnothrow	nt{} ;
+	cnullptr	np{} ;
 	cchar		*cp = getenv("_") ;
 
 	printf("argname=%s\n", ((argc > 0) ? argv[0] : "")) ;
@@ -33,6 +42,15 @@ int main(int argc,mainv argv) {
 
 	cp = getprogname() ;
 	printf("progname=%s\n", ((cp != nullptr) ? cp : "")) ;
+
+	{
+	    if (char *pbuf ; (pbuf = new(nt) char[maxpath + 1]) != np) {
+	        size_t	psize = size_t(maxpath) ;
+	        sysctlbyname("kern.procname",pbuf,&psize,np,0uz) ;
+	        printf("prcname=%s\n",pbuf) ;
+	        delete [] pbuf ;
+	    } /* end if (new) */
+	}
 }
 /* end subroutine (main) */
 
