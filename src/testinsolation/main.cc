7d1fe548e9a4a310f7412fb6b0e2d514d84ca9f0
David A.D. Morano <david.a.morano@gmail.com>
2025-08-17 09:11:29 -0400

    comments + update

--- src/testinsolation/main.cc
+++ src/testinsolation/main.cc
@@ -1,29 +1,32 @@
-/* main */
+/* main SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
 
+#include	<envstandards.h>	/* must be ordered first to configure */
 #include	<sys/types.h>
 #include	<sys/param.h>
-
 #include	<iostream>
 #include	<math.h>
-
 #include	<usystem.h>
 #include	<exitcodes.h>
-
-#include	"localmisc.h"
-
+#include	<localmisc.h>
 
 
 /* forward refernces */
 
-static double	sum_sin(double,double) ;
+local double	sum_sin(double,double) noex ;
 
 
+/* local variables */
 
 
+/* exported variables */
 
-int main(int argc,char **argv)
-{
+
+/* exported subroutines */
+
+int main(int argc,mainv argv) {
 	double	y, x ;
 	double	pi = 4.0 * atan(1) ;
 	double	inc = 0.01 ;
@@ -32,9 +35,7 @@ int main(int argc,char **argv)
 	double	avg, avg1, avg2 ;
 	double	period ;
 	double	factor ;
-
 	int	rs = SR_OK ;
-
 
 	std::cout << "pi=" << pi << "\n" ;
 
@@ -64,22 +65,16 @@ int main(int argc,char **argv)
 
 /* local subroutines */
 
-
-static double sum_sin(double lower,double upper)
-{
+local double sum_sin(double lower,double upper) noex {
 	double	x, y ;
 	double	inc = 0.01 ;
-	double	sum = 0.0 ;
-
-
+	double	sum = 0.0 ; /* return-value */
 	for (x = lower ; x < upper ; x += inc) {
 		y = sin(x) ;
 		sum += (y * inc) ;
 	} /* end for */
-
 	return sum ;
 }
 /* end subroutine (sum_sin) */
-
 
 
