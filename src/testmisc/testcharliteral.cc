06d3ff88b1e5a9a80b051dbaec2c4ac48bece1af
David A.D. Morano <david.a.morano@gmail.com>
2025-09-04 00:41:57 -0400

    restore

---
+++ src/testmisc/testcharliteral.cc
@@ -0,0 +1,84 @@
+/* termcharliteral SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* test fcuntional features of C++ */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2013-07-11, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2013 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Name:
+	main
+
+	Description:
+	I test what is a valie character literal in the latest C++
+	programming language.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be ordered first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<cstring>
+#include	<new>
+#include	<utility>
+#include	<functional>
+#include	<algorithm>
+#include	<string>
+#include	<iostream>
+#include	<iomanip>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+#include	<localmisc.h>		/* |eol(3ocal)| */
+
+
+/* local defines */
+
+
+/* name-spaces */
+
+using std::cout ;			/* variable */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int main(int,mainv,mainv) {
+    	cout << "\e[1mblinking\e[m" << eol ;
+}
+/* end subroutine (main) */
+
+
+/* local subroutines */
+
+
