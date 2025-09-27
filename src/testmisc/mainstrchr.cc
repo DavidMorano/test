a3e5d22e51ca414b22dcf28223335591f8db1c66
David A.D. Morano <david.a.morano@gmail.com>
2025-09-27 00:12:47 -0400

    garbage-clean

--- src/testmisc/mainstrchr.cc
+++ src/testmisc/mainstrchr.cc
@@ -1,44 +1,47 @@
-/* main (strchr) */
+/* main SUPPORT (strchr) */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
 /* test to see how |strchr(3c)| reacts to 8-bit character */
+/* version %I% last-modified %G% */
 
 
 /* revision history:
 
-	= 2017-09-07, David AÂ­DÂ­ Morano
+	= 2017-09-07, David A­D­ Morano
 	Originally written for Rightcore Network Services.
 
 */
 
-/* Copyright Â© 2017 David AÂ­DÂ­ Morano.  All rights reserved. */
+/* Copyright © 2017 David A­D­ Morano.  All rights reserved. */
 
 /*****************************************************************************
 
-	This test is *not* conclusive, because we do not know the signedness
-	of the type 'char' in all compilation environments.
-
-	We are also not testing our own |nchr(3dam)| function ... because we
-	already know that it works correctly! Haha.
-
+  	Description:
+	This test is *not* conclusive, because we do not know the
+	signedness of the type 'char' in all compilation environments.
+	We are also not testing our own |nchr(3dam)| function ...
+	because we already know that it works correctly! Haha.
 
 *****************************************************************************/
 
 #include	<envstandards.h>
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
 #include	<cstring>
 #include	<cstdio>
 #include	<localmisc.h>
 
 extern int	nchr(const char *,int,int) ;
 
-int main()
-{
-	const int	sch = MKCHAR('Â­') ;
+int main() {
+	const int	sch = MKCHAR('­') ;
 	int		n = 0 ;
-	const char	*a = "thisÂ­thatÂ­other" ;
+	const char	*a = "this­that­other" ;
 	const char	*sp, *tp ;
 
 	sp = a ;
-	while ((tp = strchr(sp,sch)) != NULL) {
+	while ((tp = strchr(sp,sch)) != nullptr) {
 	    n += 1 ;
 	    sp = (tp+1) ;
 	} /* end while */
