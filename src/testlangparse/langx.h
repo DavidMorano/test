ba0e894c2044cb90b5131a935e50e83dfa8d9192
David A.D. Morano <david.a.morano@gmail.com>
2025-10-10 01:55:49 -0400

    link

---
+++ src/testlangparse/langx.h
@@ -0,0 +1,35 @@
+/* langx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* computer-language processing */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	LANGX_INCLUDE
+#define	LANGX_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+#include	<langstate.h>
+#include	<langproc.h>
+
+
+#endif /* LANGX_INCLUDE */
+
+
