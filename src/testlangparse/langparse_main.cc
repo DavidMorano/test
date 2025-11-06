bf246ee2798ab150d98cf7e15ca9cc73f3997b40
David A.D. Morano <david.a.morano@gmail.com>
2025-11-06 00:37:34 -0500

    update

--- src/testlangparse/langparse_main.cc
+++ src/testlangparse/langparse_main.cc
@@ -11,9 +11,19 @@
 	= 1998-04-13, David A-D- Morano
 	Originally written for Rightcore Network Services.
 
+	= 2025-11-03, David A-D- Morano
+	I am updating this to the latest API of the LANGPARSE object.
+
 */
 
-/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Copyright © 1998,2025 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Description:
+	I test the LANGPARSE object.
+
+*******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<sys/types.h>
@@ -32,6 +42,10 @@
 #include	<localmisc.h>		/* |eol| */
 
 #include	<langparse.h>
+
+#pragma		GCC dependency		"mod/libutil.ccm"
+
+import libutil ;			/* |getlenstr(3u)| */
 
 /* local defines */
 
@@ -58,6 +72,7 @@ using std::cerr ;			/* variable */
 
 local int procfile(langparse *,cchar *) noex ;
 local int procln(langparse *,cchar *,int) noex ;
+local int procout(langparse *,int) noex ;
 
 
 /* local variables */
@@ -73,7 +88,7 @@ int main(int argc,mainv argv,mainv) {
 	int		rs = SR_OK ;
 	int		rs1 ;
 	if (argc > 0) {
-	    if (langparse pa ; (rs = pa.start) >= 0) {
+	    if (langparse pa ; (rs = pa.start) >= 0) ylikely {
 	        for (int ai = 1 ; (ai < argc) && argv[ai] ; ai += 1) {
 		    cchar	*fn = argv[ai] ;
 		    rs = procfile(&pa,fn) ;
@@ -97,10 +112,12 @@ local int procfile(langparse *lpp,cchar *fn) noex {
 	cint		llen = LINEBUFLEN ;
     	int		rs = SR_FAULT ;
 	int		rs1 ;
-	if (char *lbuf ; (lbuf = new(nt) char[llen + 1]) != np) {
-	    if (ccfile rf ; (rs = rf.open(fn,"r",0)) >= 0) {
+	if (char *lbuf ; (lbuf = new(nt) char[llen + 1]) != np) ylikely {
+	    if (ccfile rf ; (rs = rf.open(fn,"r",0)) >= 0) ylikely {
 		while ((rs = rf.readln(lbuf,llen)) > 0) {
-		    rs = procln(lpp,lbuf,rs) ;
+		    if ((rs = procln(lpp,lbuf,rs)) > 0) {
+			rs = procout(lpp,llen) ;
+		    }
 		    if (rs < 0) break ;
 		} /* end while */
 		rs1 = rf.close ;
@@ -112,11 +129,32 @@ local int procfile(langparse *lpp,cchar *fn) noex {
 } /* end subroutine (procfile) */
 
 local int procln(langparse *lpp,cchar *lp,int ll) noex {
-    	int		rs = SR_OK ;
-	(void) lpp ;
-	(void) lp ;
-	(void) ll ;
-	return rs ;
+    	int		rs ;
+	int		c = 0 ;
+	if ((rs = getlenstr(lp,ll)) > 0) {
+	    rs = lpp->load(lp,ll) ;
+	    c = rs ;
+	} /* end if (getlenstr) */
+	return (rs >= 0) ? c : rs ;
 } /* end subroutine (procln) */
+
+local int procout(langparse *lpp,int slen) noex {
+    	cnullptr	np{} ;
+    	cnothrow	nt{} ;
+    	int		rs = SR_OK ;
+	int		wlen = 0 ;
+	if (short *sb ; (sb = new(nt) short [slen + 1]) != np) {
+	    while ((rs = lpp->read(sb,slen)) > 0) {
+		for (int i = 0 ; i < rs ; i += 1) {
+		    char	ch = char(sb[i]) ;
+		    cout << ch ;
+		    wlen += 1 ;
+		} /* end for */
+		cout << eol ;
+	    } /* end while */
+	    delete [] sb ;
+	} /* end if (new-short) */
+	return (rs >= 0) ? wlen : rs ;
+} /* end subroutine (procout) */
 
 
