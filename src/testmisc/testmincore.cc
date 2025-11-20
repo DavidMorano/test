ef1f3f9ca59bf247b728a6b65e03379bdd8230a0
David A.D. Morano <david.a.morano@gmail.com>
2025-11-20 01:04:54 -0500

    restore

---
+++ src/testmisc/testmincore.cc
@@ -0,0 +1,46 @@
+/* testmincore SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C89 */
+
+#define	CF_DEBUGS	1		/* compile-time debugging */
+#define	CF_DEBUGMALL	1		/* debugging memory-allocations */
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<cstdarg>
+#include	<cstdio>
+#include	<usystem.h>
+#include	<localmisc.h>		/* TIMEBUFLEN */
+
+/* local defines */
+
+
+/* external subroutines */
+
+
+/* forward references */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int main(int argc,mainv,mainv) {
+	int		rs = SR_OK ;
+	int		rs1 ;
+
+
+
+	if (rs < 0) {
+	    printf("failure (%d)\n",rs) ;
+	}
+
+}
+/* end subroutine (main) */
+
+
+/* local subroutines */
+
+
