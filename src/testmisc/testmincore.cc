c1dbe5b3cbd1c17644400ded526ae58c6e1a4c48
David A.D. Morano <david.a.morano@gmail.com>
2025-11-21 00:56:31 -0500

    update

--- src/testmisc/testmincore.cc
+++ src/testmisc/testmincore.cc
@@ -1,9 +1,28 @@
 /* testmincore SUPPORT */
 /* charset=ISO8859-1 */
-/* lang=C89 */
+/* lang=C++20 (conformance reviewed) */
 
-#define	CF_DEBUGS	1		/* compile-time debugging */
-#define	CF_DEBUGMALL	1		/* debugging memory-allocations */
+/* test the |mincore(2)| UNIX® (POSIX®) system call */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-13, David A-D- Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
+
+/*******************************************************************************
+
+  	Description:
+	This is test code (in the present case for the |mincore(2)|
+	system call).
+
+*******************************************************************************/
 
 #include	<envstandards.h>	/* ordered first to configure */
 #include	<cstddef>		/* |nullptr_t| */
@@ -13,13 +32,30 @@
 #include	<usystem.h>
 #include	<localmisc.h>		/* TIMEBUFLEN */
 
+#pragma		GCC dependency		"mod/libutil.ccm"
+#pragma		GCC dependency		"mod/ulibvals.ccm"
+
+import ulibvals ;			/* |ulibval(3u)| */
+
 /* local defines */
+
+
+/* imported namespaces */
 
 
 /* external subroutines */
 
 
+/* external variables */
+
+
+/* local structures */
+
+
 /* forward references */
+
+
+/* local variables */
 
 
 /* exported variables */
@@ -28,6 +64,7 @@
 /* exported subroutines */
 
 int main(int argc,mainv,mainv) {
+    	cint		ps = ulibval.pagesz ;
 	int		rs = SR_OK ;
 	int		rs1 ;
 
@@ -36,7 +73,8 @@ int main(int argc,mainv,mainv) {
 	if (rs < 0) {
 	    printf("failure (%d)\n",rs) ;
 	}
-
+	(void) ps ;
+	(void) rs1 ;
 }
 /* end subroutine (main) */
 
