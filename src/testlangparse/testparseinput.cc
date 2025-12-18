b71bf2d1bcaa19aee9719cd76268b29feebc01d8
David A.D. Morano <david.a.morano@gmail.com>
2025-12-18 00:21:26 -0500

    restore

---
+++ src/testlangparse/testparseinput.cc
@@ -0,0 +1,28 @@
+/* testlangparse (test language parsing) */
+
+
+#include	<envstandards.h>	/* must be ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+#include	<usyscalls.h>
+#include	<exitcodes.h>		/* |EX_{xxx}| */
+#include	<localmisc.h>
+
+/* hello "*/
+/* goodbye \*/
+
+/*
+#include	"langx.h"
+	hello there "" */
+
+int main(int,mainv,mainv) {
+    	int		ex = EX_OK ;
+	cchar		*rsp = R"abc(some \"'()junk)xbc")abc)abc" ;
+	(void) rsp ;
+	return ex ;
+} /* end subroutine (main) */
+
+
