ca4cdc02578e41c2a522547b20e76cad9409d87f
David A.D. Morano <david.a.morano@gmail.com>
2025-12-18 00:21:52 -0500

    restore

---
+++ src/testlangparse/v4langparse.cc
@@ -0,0 +1,441 @@
+/* langparse SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
+
+/* Language-Parse (replacement for LANGSTATE) */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2016-06-29, David A­D­ Morano
+	This was made from scratch, although it is (very) similar to
+	several others that I have made like it in the past.  The
+	problem that this code is *supposed* to solve has been
+	handled (poorly) for a long time now.
+
+	= 2018-12-14, David A-D- Morano
+	Made a small enhancement for error handling.
+
+*/
+
+/* Copyright @ 2016,2018 David A-D- Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Object:
+	langparse
+
+	Names:
+	langparse_start
+	langparse_load
+	langparse_read
+	langparse_finish
+
+	Description:
+	This object facilitates the parsing of C and C++ language
+	source text.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<string>		/* C++ |string| */
+#include	<new>			/* |nothrow(3c++)| */
+#include	<algorithm>		/* |min(3c++)| + |max(3c++)| */
+#include	<usystem.h>
+#include	<six.h>			/* |sichr(2uc)| */
+#include	<shortq.h>
+#include	<strwcpy.h>
+#include	<ascii.h>
+#include	<mkchar.h>
+#include	<char.h>
+#include	<ischarx.h>
+#include	<localmisc.h>
+
+#include	"langparse.h"
+
+#pragma		GCC dependency		"mod/libutil.ccm"
+
+import libutil ;			/* |getlenstr(3u)| */
+
+/* local defines */
+
+
+/* namespaces */
+
+using std::string ;			/* type */
+using std::min ;			/* subroutine-template */
+using std::max ;			/* subroutine-template */
+using std::nothrow ;			/* constant */
+
+
+/* local typedefs */
+
+typedef shortq *	obufp ;
+typedef string *	strp ;
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+template<typename ... Args>
+local int langparse_ctor(langparse *op,Args ... args) noex {
+	LANGPARSE	*hop = op ;
+	cnothrow	nt{} ;
+	cnullptr	np{} ;
+	int		rs = SR_FAULT ;
+	if (op && (args && ...)) ylikely {
+	    memclear(hop) ;
+	    rs = SR_NOMEM ;
+	    if (string *sp ; (sp = new(nt) string) != np) ylikely {
+		op->rstrp = sp ;
+		rs = SR_OK ;
+	    } /* end if (new-string) */
+	} /* end if (non-null) */
+	return rs ;
+}
+/* end subroutine (langparse_ctor) */
+
+local int langparse_dtor(langparse *op) noex {
+	int		rs = SR_FAULT ;
+	if (op) ylikely {
+	    rs = SR_OK ;
+	    if (op->rstrp) ylikely {
+		string *sp = strp(op->rstrp) ;
+		delete sp ;
+	    }
+	} /* end if (non-null) */
+	return rs ;
+}
+/* end subroutine (langparse_dtor) */
+
+template<typename ... Args>
+local inline int langparse_magic(langparse *op,Args ... args) noex {
+	int		rs = SR_FAULT ;
+	if (op && (args && ...)) ylikely {
+	    rs = (op->magic == LANGPARSE_MAGIC) ? SR_OK : SR_NOTOPEN ;
+	}
+	return rs ;
+}
+/* end subroutine (langparse_magic) */
+
+local int	langparse_proc(langparse *,int) noex ;
+local int	langparse_clear(langparse *,int) noex ;
+local int	langparse_strreg(langparse *,int) noex ;
+local int	langparse_strraw(langparse *,int) noex ;
+local int	langparse_literal(langparse *,int) noex ;
+local int	langparse_comment(langparse *,int) noex ;
+local int	langparse_store(langparse *,int) noex ;
+
+#ifdef	COMMENT
+local int	langparse_add(langparse *,cchar *,int) noex ;
+#endif /* COMMENT */
+
+
+/* local variables */
+
+cint		nstage = LANGPARSE_NSTAGE ;
+
+
+/* exported variables */
+
+int langparsems::comment	= (1 << langparseo_comment) ;
+int langparsems::strreg		= (1 << langparseo_strreg) ;
+int langparsems::strraw		= (1 << langparseo_strraw) ;
+int langparsems::literal	= (1 << langparseo_literal) ;
+
+const langparsems		langparsem ;
+
+
+/* exported subroutines */
+
+int langparse_start(langparse *op) noex {
+    	cnothrow	nt{} ;
+	cnullptr	np{} ;
+	int		rs ;
+	if ((rs = langparse_ctor(op)) >= 0) ylikely {
+	    if (shortq *obp ; (obp = new(nt) shortq) != np) ylikely {
+		if ((rs = obp->start) >= 0) ylikely {
+	            op->outbuf = obp ;
+		    op->fl.clear = true ;	/* initial state */
+	            op->magic = LANGPARSE_MAGIC ;
+		}
+		if (rs < 0) {
+		    delete obp ;
+		} /* end if (error) */
+	    } else {
+	        rs = SR_NOMEM ;
+	    } /* end if (new-shortq) */
+	    if (rs < 0) {
+		langparse_dtor(op) ;
+	    }
+	} /* end if (langparse_ctor) */
+	return rs ;
+}
+/* end subroutine (langparse_start) */
+
+int langparse_finish(langparse *op) noex {
+	int		rs ;
+	int		rs1 ;
+	if ((rs = langparse_magic(op)) >= 0) ylikely {
+	    if (shortq *obp = obufp(op->outbuf) ; obp) ylikely {
+		{
+		    rs1 = obp->finish ;
+		    if (rs >= 0) rs = rs1 ;
+		}
+		{
+	            delete obp ;
+	            op->outbuf = nullptr ;
+		}
+	    } /* end if (outbuf) */
+	    {
+		rs1 = langparse_dtor(op) ;
+		if (rs >= 0) rs = rs1 ;
+	    }
+	    op->magic = 0 ;
+	} /* end if (magic) */
+	return rs ;
+}
+/* end subroutine (langparse_finish) */
+
+int langparse_load(langparse *op,cchar *sp,int µsl) noex {
+	int		rs ;
+	int		c = 0 ;
+	if ((rs = langparse_magic(op,sp)) >= 0) ylikely {
+	    if (int sl ; (sl = getlenstr(sp,µsl)) >= 0) ylikely {
+	        while (sl-- && *sp) {
+		    cint ch = mkchar(*sp++) ;
+		    rs = langparse_proc(op,ch) ;
+		    c += 1 ;
+		    if (rs < 0) break ;
+	        } /* end while */
+	    } /* end if (getlenstr) */
+	} /* end if (non-null) */
+	return (rs >= 0) ? c : rs ;
+}
+/* end subroutine (langparse_load) */
+
+int langparse_read(langparse *op,short *rbuf,int rlen) noex {
+    	cnullptr	np{} ;
+	int		rs ;
+	int		i = 0 ; /* return-value */
+	if ((rs = langparse_magic(op,rbuf)) >= 0) ylikely {
+	    rs = SR_INVALID ;
+	    rbuf[0] = 0 ;
+	    if (rlen >= 0) ylikely {
+	        int	ml ;
+	        if (shortq *obp ; (obp = obufp(op->outbuf)) != np) ylikely {
+	            cint	len = obp->len ;
+	            ml = min(len,rlen) ;
+	            for (i = 0 ; i < ml ; i += 1) {
+			if (short code ; (rs = obp->rem(&code)) >= 0) {
+	                    rbuf[i] = code ;
+			} else if (rs == SR_NOTFOUND) {
+			    rs = SR_OK ;
+			    break ;
+			}
+	            } /* end for */
+	            rbuf[i] = 0 ;
+	        } else {
+	            rs = SR_BUGCHECK ;
+	        }
+	    } /* end if (positive) */
+	} /* end if (non-null) */
+	return (rs >= 0) ? i : rs ;
+}
+/* end subroutine (langparse_read) */
+
+
+/* private subroutines */
+
+local int langparse_proc(langparse *op,int ch) noex {
+	int		rs = SR_OK ;
+	if (op->fl.clear) {
+	    if (op->fl.strreg) {
+		rs = langparse_strreg(op,ch) ;
+	    } else if (op->fl.strraw) {
+		rs = langparse_strraw(op,ch) ;
+	    } else if (op->fl.literal) {
+		rs = langparse_literal(op,ch) ;
+	    } else {
+		rs = langparse_clear(op,ch) ;
+	    }
+	} else if (op->fl.comment) {
+	    rs = langparse_comment(op,ch) ;
+	}
+	op->pch = ch ;
+	return rs ;
+} /* end subroutine (langparse_proc) */
+
+local int langparse_clear(langparse *op,int ch) noex {
+	const langparsems	&lm = langparsem ;
+    	int		rs = SR_OK ;
+	bool		fstore = true ;
+	switch (ch) {
+	case CH_SLASH:
+	    if (op->pch != CH_BSLASH) {
+	        fstore = false ;
+	    }
+	    break ;
+	case 'R':
+            if (op->pch != CH_BSLASH) {
+	        fstore = false ;
+	    } /* end if (entering a raw string-literal) */
+            break ;
+        case CH_STAR:
+            if (op->pch == CH_SLASH) {
+		cint chm = (op->pch | lm.comment) ;
+                op->fl.comment = true ;
+                op->fl.clear = false ;
+		rs = langparse_store(op,chm) ;
+		ch |= lm.comment ;
+            } /* end if (entering comment) */
+            break ;
+        case CH_DQUOTE:
+	    if (op->pch == 'R') {
+		cint chm = (op->pch | lm.strraw) ;
+                op->fl.strraw = true ;
+		rs = langparse_store(op,chm) ;
+		ch |= lm.strraw ;
+	    } else if ((op->pch != CH_BSLASH) && (op->pch != CH_SQUOTE)) {
+                op->fl.strreg = true ;
+		ch |= lm.strreg ;
+            } /* end if (entering a regular quoted string) */
+            break ;
+        case CH_SQUOTE:
+            if (op->pch != CH_BSLASH) {
+                op->fl.literal = true ;
+		ch |= lm.literal ;
+            } /* end if (entering a literal) */
+	    break ;
+	default:
+	    if (int chx = op->pch ; (chx == CH_SLASH) || (chx == 'R')) {
+		rs = langparse_store(op,chx) ;
+	    }
+	    break ;
+	} /* end switch */
+	if ((rs >= 0) && fstore) {
+	    rs = langparse_store(op,ch) ;
+	} /* end if (store character) */
+	return rs ;
+} /* end subrolutine (langparse_clear) */
+
+local int langparse_strreg(langparse *op,int ch) noex {
+	const langparsems	&lm = langparsem ;
+    	int		rs = SR_OK ;
+	if ((op->pch != CH_BSLASH) && (ch == CH_DQUOTE)) {
+	    op->fl.strreg = false ;
+	}
+	if (rs >= 0) {
+	    cint chm = (ch | lm.strreg) ;
+	    rs = langparse_store(op,chm) ;
+	} /* end if (store character) */
+	return rs ;
+} /* end subrolutine (langparse_strreg) */
+
+local int langparse_strraw(langparse *op,int ch) noex {
+	const langparsems	&lm = langparsem ;
+    	int		rs = SR_OK ;
+	if (rs >= 0) {
+	    cint chm = (ch | lm.strreg) ;
+	    rs = langparse_store(op,chm) ;
+	} /* end if (store character) */
+	return rs ;
+} /* end subrolutine (langparse_strraw) */
+
+local int langparse_literal(langparse *op,int ch) noex {
+	const langparsems	&lm = langparsem ;
+    	int		rs = SR_OK ;
+	if ((op->pch != CH_BSLASH) && (ch == CH_SQUOTE)) {
+	    op->fl.literal = false ;
+	}
+	if (rs >= 0) {
+	    cint chm = (ch | lm.literal) ;
+	    rs = langparse_store(op,chm) ;
+	} /* end if (store character) */
+	return rs ;
+} /* end subrolutine (langparse_literal) */
+
+local int langparse_comment(langparse *op,int ch) noex {
+	const langparsems	&lm = langparsem ;
+    	int		rs = SR_OK ;
+	if ((op->pch == CH_STAR) && (ch == CH_SLASH)) {
+	    op->fl.comment = false ;
+	    op->fl.clear = true ;
+	}
+	if (rs >= 0) {
+	    cint chm = (ch | lm.comment) ;
+	    rs = langparse_store(op,chm) ;
+	} /* end if (store character) */
+	return rs ;
+} /* end subrolutine (langparse_comment) */
+
+local int langparse_store(langparse *op,int v) noex {
+    	int		rs = SR_BUGCHECK ;
+	if (shortq *obp = obufp(op->outbuf) ; obp) ylikely {
+	    cshort code = shortconv(v) ;
+	    rs = obp->ins(code) ;
+	}
+	return (rs >= 0) ? 1 : rs ;
+} /* end subrolutine (langparse_store) */
+
+int langparse::load(cchar *sp,int sl) noex {
+	return langparse_load(this,sp,sl) ;
+}
+
+int langparse::read(short *rbuf,int rlen) noex {
+	return langparse_read(this,rbuf,rlen) ;
+}
+
+#ifdef	COMMENT
+local int langparse_add(langparse *op,cchar *vp,int vl = -1) noex {
+	int		rs = SR_OK ;
+	int		c = 0 ;
+	while ((rs >= 0) && vl-- && *vp) {
+	    rs = langparse_add(op,*vp++) ;
+	    c += rs ;
+	}
+	return (rs >= 0) ? c : rs ;
+} /* end subrolutine (langparse_add) */
+#endif /* COMMENT */
+
+void langparse::dtor() noex {
+	if (cint rs = finish ; rs < 0) {
+	    ulogerror("langparse",rs,"fini-finish") ;
+	}
+} /* end method (langparse::dtor) */
+
+langparse::operator int () noex {
+    	int		rs ;
+	if ((rs = langparse_magic(this)) >= 0) ylikely {
+	    rs = 0 ;
+	}
+	return rs ;
+} /* end method (langparse::operator) */
+
+langparse_co::operator int () noex {
+	int		rs = SR_BUGCHECK ;
+	if (op) ylikely {
+	    switch (w) {
+	    case langparsemem_start:
+	        rs = langparse_start(op) ;
+	        break ;
+	    case langparsemem_finish:
+	        rs = langparse_finish(op) ;
+	        break ;
+	    } /* end switch */
+	} /* end if (non-null) */
+	return rs ;
+} /* end method (langparse_co::operator) */
+
+
