29df0d0da859572b363ccfeca0c081d8748086cd
David A.D. Morano <david.a.morano@gmail.com>
2026-01-11 02:32:27 -0500

    update

--- src/testvecsorthand/vecsorthand.h
+++ src/testvecsorthand/vecsorthand.h
@@ -21,10 +21,7 @@
 
 #include	<envstandards.h>	/* MUST be ordered first to configure */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 
 
 #define	VECSORTHAND		struct vecsorthand_head
