37e3ffe58a3abcd8f3ac407a8194b8ed12067a02
David A.D. Morano <david.a.morano@gmail.com>
2026-01-11 02:33:59 -0500

    restore

---
+++ src/testvecelem/debug.h
@@ -0,0 +1,32 @@
+/* debug HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* cheapo debugging */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	DEBUG_INCLUDE
+#define	DEBUG_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+
+#endif /* DEBUG_INCLUDE */
+
+
