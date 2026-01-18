310ca38c7c3b63b8505814e0a8370556982f7a60
David A.D. Morano <david.a.morano@gmail.com>
2026-01-18 00:24:25 -0500

    update

--- src/testmisc/testcotd.cc
+++ src/testmisc/testcotd.cc
@@ -29,8 +29,6 @@
 
 /* external subroutines */
 
-extern "C" int	main(int,const char **,const char **) ;
-
 #if	CF_DEBUGS
 extern "C" int	debugopen(const char *) ;
 extern "C" int	debugprintf(const char *,...) ;
@@ -38,11 +36,8 @@ extern "C" int	debugclose() ;
 extern "C" int	strlinelen(const char *,int,int) ;
 #endif
 
-extern "C" const char	*getourenv(const char **,const char *) ;
 
-
-int main(int argc,const char **argv,const char **envv)
-{
+int main(int argc,const char **argv,const char **envv) {
 	const int	ofd = FD_STDOUT ;
 	int		rs ;
 	const char	*fn = "local§cotd" ;
