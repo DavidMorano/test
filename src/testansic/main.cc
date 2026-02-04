b7d8e7f647c898760a26051d300f0a97bfde7c91
David A.D. Morano <david.a.morano@gmail.com>
2026-02-04 00:45:02 -0500

    update

--- src/testansic/main.cc
+++ src/testansic/main.cc
@@ -1,4 +1,5 @@
-/* main */
+/* main SUPPORT (testansic) */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* see if you have an ANSI compiler or the K&R compiler */
@@ -8,16 +9,27 @@
 /* revision history:
 
 	= 1989-03-01, David A­D­ Morano
-	This was written way back when the ANSI-standardized C-language compiler
-	first came out. I am testing how the new compiler messes up integral
-	promotions in the case of signed inetegers meeting unsigned-integers.
+	This was written way back when the ANSI-standardized
+	C-language compiler first came out. I am testing how the
+	new compiler messes up integral promotions in the case of
+	signed inetegers meeting unsigned-integers.
 
 */
 
+/* Copyright © 1989 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
 
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>		/* |getenv(3c)| */
 #include	<cstdio>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>
+#include	<uclibmem.h>
+#include	<localmisc.h>
 
-int main(int,const char **,const char **) {
+int main(int,mainv,mainv) {
 	int		iw = -1 ;
 	unsigned short	us = 1 ;
 	const char	*fmt ;
