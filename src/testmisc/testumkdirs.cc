7731d22b06c23831d351d00d19e125d74aba24f3
David A.D. Morano <david.a.morano@gmail.com>
2026-02-12 00:48:45 -0500

    restore

---
+++ src/testmisc/testumkdirs.cc
@@ -0,0 +1,50 @@
+/* testumkdirs_main SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* test the |u_mkdirs(3u)| subroutine */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2000-05-14, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>		/* |getenv(3c)| */
+#include	<cstdio>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>		/* |u_mkdirs(3u)| */
+#include	<localmisc.h>
+
+#pragma		GCC dependency		"mod/libutil.ccm"
+
+import libutil ;			/* |lenstr(3u)| */
+
+int main(int argc,mainv argv,mainv) {
+	int		rs = SR_OK ;
+	int		ex = EXIT_SUCCESS ;
+	if (argc > 1) {
+	    cmode dm = 0775 ;
+	    for (int ai = 1 ; (ai < argc) && argv[ai] ; ai += 1) {
+		if (cchar *fn = argv[ai] ; fn[0]) {
+		    rs = u_mkdirs(fn,dm) ;
+		}
+		if (rs < 0) break ;
+	    } /* end for */
+	} /* end block */
+	if ((rs < 0) && (ex == EXIT_SUCCESS)) {
+	    ex = EXIT_FAILURE ;
+	}
+	return ex ;
+}
+/* end subroutine (main) */
+
+
