f071ce821ceb6dd4ec5ace52040c554f5b2fbc06
David A.D. Morano <david.a.morano@gmail.com>
2026-02-12 00:49:12 -0500

    update

--- src/testumkdirs/testumkdirs_main.cc
+++ src/testumkdirs/testumkdirs_main.cc
@@ -16,12 +16,9 @@
 /* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<sys/types.h>
-#include	<sys/time.h>
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>		/* |getenv(3c)| */
 #include	<cstdio>
-#include	<cstring>		/* |strncmp(3c)| */
 #include	<clanguage.h>
 #include	<usysbase.h>
 #include	<usyscalls.h>		/* |u_mkdirs(3u)| */
