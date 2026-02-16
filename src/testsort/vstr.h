f1b77d2e81d6a7fd76f100d19c6032bb3e97ae90
David A.D. Morano <david.a.morano@gmail.com>
2026-02-16 00:09:04 -0500

    restore

---
+++ src/testsort/vstr.h
@@ -0,0 +1,36 @@
+/* vstr HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* v-string key-comparison functions */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	VSTRKEYCMPX_INCLUDE
+#define	VSTRKEYCMPX_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+#include	<vstrcmp.h>
+#include	<vstrkeycmp.h>
+#include	<vstrkeydictcmp.h>
+#include	<vstrsort.h>
+
+
+#endif /* VSTRKEYCMPX_INCLUDE */
+
+
