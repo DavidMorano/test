c71bec03c85dbef95e5abd6b73253d42ec69abe8
David A.D. Morano <david.a.morano@gmail.com>
2026-03-01 00:52:37 -0500

    update

--- src/testfindbit/main.cc
+++ src/testfindbit/main.cc
@@ -1,13 +1,17 @@
-/* main (testfindbit) */
+/* main SUPPOER (testfindbit) */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
 
+/* testing the "find-bit" operations */
+/* version %I% last-modified %G% */
 
 #define	CF_DEBUGS	0		/* compile-time debugging */
-
 
 /* revision history:
 
 	= 1998-11-01, David A­D­ Morano
-	This subroutine was written for Rightcore Network Services (RNS).
+	This subroutine was written for Rightcore Network Services
+	(RNS).
 
 */
 
@@ -15,24 +19,23 @@
 
 /*******************************************************************************
 
-        This little program tests the 'findbit(3dam)' set of subroutines.
-
+  	Description:
+	This little program tests the |findbit(3dam)| set of
+	subroutines.
 
 *******************************************************************************/
 
-
-#include	<envstandards.h>
-
-#include	<sys/types.h>
-#include	<sys/param.h>
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<climits>
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>		/* |getenv(3c)| */
 #include	<cstdio>
-
+#include	<clanguage.h>
+#include	<usysbase.h>
 #include	<ascii.h>
 #include	<findbit.h>
 #include	<xfindbit.h>
 #include	<random.h>
-
 #include	<localmisc.h>
 
 
@@ -41,35 +44,41 @@
 #define	NLOOPS		(1*1024*1024)
 
 
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
 /* forward references */
 
-static int	testffbs(RANDOM *) ;
-static int	testflbs(RANDOM *) ;
-static int	testffbc(RANDOM *) ;
-static int	testflbc(RANDOM *) ;
+local int	testffbs(RANDOM *) ;
+local int	testflbs(RANDOM *) ;
+local int	testffbc(RANDOM *) ;
+local int	testflbc(RANDOM *) ;
+
+
+/* local variables */
+
+
+/* exported variables */
 
 
 /* exported subroutines */
 
-
-int main()
-{
-	RANDOM	r ;
+int main(int,mainv,mainv) {
 	int	rs ;
-
-	if ((rs = random_start(&r,1,0)) >= 0) {
-
+	if (RANDOM r ; (rs = random_start(&r,1,0)) >= 0) {
 	    testffbs(&r) ;
 	    testflbs(&r) ;
-
 	    testffbc(&r) ;
 	    testflbc(&r) ;
-
 	    random_finish(&r) ;
 	} /* end if (random) */
-
 	fprintf(stdout,"done\n") ;
-
 	return 0 ;
 }
 /* end subroutine (main) */
@@ -77,106 +86,62 @@ int main()
 
 /* local subroutines */
 
-
-static int testffbs(RANDOM *rp)
-{
+local int testffbs(RANDOM *rp) {
 	uint	v ;
-
-	int	i ;
 	int	n1, n2 ;
-
-
-	for (i = 0 ; i < NLOOPS ; i += 1) {
-
+	for (int i = 0 ; i < NLOOPS ; i += 1) {
 	    random_getuint(rp,&v) ;
-
 	    n1 = ffbsi(v) ;
-
 	    n2 = xffbsi(v) ;
-
-	    if (n1 != n2)
+	    if (n1 != n2) {
 		fprintf(stdout,"FFBS bad v=%08lx n1=%u n2=%u\n",v,n1,n2) ;
-
+	    }
 	} /* end for */
-
 	return 0 ;
 }
 /* end subroutine (testffbs) */
 
-
-static int testflbs(RANDOM *rp)
-{
+local int testflbs(RANDOM *rp) {
 	uint	v ;
-
-	int	i ;
 	int	n1, n2 ;
-
-
-	for (i = 0 ; i < NLOOPS ; i += 1) {
-
+	for (int i = 0 ; i < NLOOPS ; i += 1) {
 	    random_getuint(rp,&v) ;
-
 	    n1 = flbsi(v) ;
-
 	    n2 = xflbsi(v) ;
-
-	    if (n1 != n2)
+	    if (n1 != n2) {
 		fprintf(stdout,"FLBS bad v=%08lx n1=%u n2=%u\n",v,n1,n2) ;
-
+	    }
 	} /* end for */
-
 	return 0 ;
 }
 /* end subroutine (testflbs) */
 
-
-static int testffbc(RANDOM *rp)
-{
+local int testffbc(RANDOM *rp) {
 	uint	v ;
-
-	int	i ;
 	int	n1, n2 ;
-
-
-	for (i = 0 ; i < NLOOPS ; i += 1) {
-
+	for (int i = 0 ; i < NLOOPS ; i += 1) {
 	    random_getuint(rp,&v) ;
-
 	    n1 = ffbci(v) ;
-
 	    n2 = xffbci(v) ;
-
-	    if (n1 != n2)
+	    if (n1 != n2) {
 		fprintf(stdout,"FFBC bad v=%08lx n1=%u n2=%u\n",v,n1,n2) ;
-
+	    }
 	} /* end for */
-
 	return 0 ;
 }
 /* end subroutine (testffbc) */
 
-
-static int testflbc(RANDOM *rp)
-{
+local int testflbc(RANDOM *rp) {
 	uint	v ;
-
-	int	i ;
 	int	n1, n2 ;
-
-
-	for (i = 0 ; i < NLOOPS ; i += 1) {
-
+	for (int i = 0 ; i < NLOOPS ; i += 1) {
 	    random_getuint(rp,&v) ;
-
 	    n1 = flbci(v) ;
-
 	    n2 = xflbci(v) ;
-
-	    if (n1 != n2)
+	    if (n1 != n2) {
 		fprintf(stdout,"FLBC bad v=%08lx n1=%u n2=%u\n",v,n1,n2) ;
-
+	    }
 	} /* end for */
-
 	return 0 ;
 }
 /* end subroutine (testflbc) */
