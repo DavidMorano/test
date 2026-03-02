85f4b43b591d5c9678dab78a514e54d8ce6c0237
David A.D. Morano <david.a.morano@gmail.com>
2026-03-02 00:25:03 -0500

    restore

---
+++ src/testfsdirtree/testfsdirtree.h
@@ -0,0 +1,29 @@
+/* testfsdirtree HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* traverse directory tree */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	TESTFSDIRTREE_INCLUDE
+#define	TESTFSDIRTREE_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+#endif /* TESTFSDIRTREE_INCLUDE */
+
+
