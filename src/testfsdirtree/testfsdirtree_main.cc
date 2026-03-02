6355cd74d32c1f4f03c285d7e41955b4c4412516
David A.D. Morano <david.a.morano@gmail.com>
2026-03-02 00:24:59 -0500

    restore

---
+++ src/testfsdirtree/testfsdirtree_main.cc
@@ -0,0 +1,79 @@
+/* textfsdirtree_main SUPPORT (testfsdirtree) */
+/* charset=ISO8859-1 */
+/* lang=C++98 */
+
+/* test calling once */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2000-05-14, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<sys/stat.h>		/* |stat(2)| */
+#include	<unistd.h>		/* |getpid(2)| */
+#include	<fcntl.h>
+#include	<libproc.h>
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>		/* |getenv(3c)| + |getprogname(3c)| */
+#include	<cstdio>
+#include	<new>			/* |nothrow(3c++)| */
+#include	<iostream>		/* |cout| */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+#include	<usyscalls.h>
+#include	<usupport.h>
+#include	<fsdirtree.h>
+#include	<localmisc.h>		/* |MAXNAMELEN| + |eol| */
+
+import libutil ;
+
+using libu::umem ;			/* variable */
+using std::cout ;			/* variable */
+using std::nothrow ;			/* constant */
+
+local int dirlist(cchar *name) noex {
+    	cnullptr	np{} ;
+    	int		rs = SR_INVALID ;
+	int		rs1 ;
+	if (name[0]) {
+	    cint	nlen = MAXNAMELEN ;
+	    rs = SR_NOMEM ;
+	    if (char *nbuf ; (nbuf = new(nothrow) char[nlen + 1]) != np) {
+	        if (fsdirtree dir ; (rs = dir.open(name)) >= 0) {
+		    for (ustat sb ; (rs = dir.read(&sb,nbuf,nlen)) > 0 ; ) {
+			cout << nbuf << eol ;
+		    } /* end while */
+		    rs1 = dir.close ;
+		    if (rs >= 0) rs = rs1 ;
+	        } /* end if (fsdirtree) */
+	        delete [] nbuf ;
+	    } /* end if (m-a-f) */
+	}
+	return rs ;
+} /* end subroutine (dirlist) */
+
+int main(int argc,mainv argv,mainv) {
+    	int		ex = 0 ;
+	int		rs = SR_OK ;
+	for (int ai = 1 ; (rs >= 0) && (ai < argc) ; ai += 1) {
+	    if (argv[ai]) {
+		cchar	*dirname = argv[ai] ;
+		rs = dirlist(dirname) ;
+	    }
+	} /* end for */
+	if (rs < 0) ex = 1 ;
+	return ex ;
+}
+/* end subroutine (main) */
+
+
