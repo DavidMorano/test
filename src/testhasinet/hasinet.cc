d3733d547a4a0ba590e7b5ff9e607e991800f867
David A.D. Morano <david.a.morano@gmail.com>
2026-03-06 00:34:48 -0500

    update

--- src/testhasinet/hasinet.cc
+++ src/testhasinet/hasinet.cc
@@ -107,10 +107,12 @@ bool hasinet4addrstr(cchar *sp,int 탎l) noex {
 	    DPRINTF("ent s=>%s<\n",ccp(ds)) ;
 	}
 	if (int sl ; (sl = getlenstr(sp,탎l)) > 0) ylikely {
+	    int		n = 0 ;
 	    int		c = 0 ;
 	    f = true ;
 	    for (cc *tp ; (tp = strnchr(sp,sl,'.')) != nullptr ; ) {
 	        cint tl = intconv(tp - sp) ;
+		n += 1 ;
 	        f = hasINET4num(sp,tl) ;
 	        if (! f) break ;
 	        sl -= intconv((tp + 1) - sp) ;
@@ -124,7 +126,7 @@ bool hasinet4addrstr(cchar *sp,int 탎l) noex {
 	            f = hasINET4num(sp,sl) ;
 		}
 	    } /* end if */
-	    f = f && (c > 0) ;
+	    f = f && (n <= 3) && (c > 0) ;
 	} /* end if (getlenstr) */
 	DPRINTF("ret f=%u\n",f) ;
 	return f ;
@@ -132,26 +134,57 @@ bool hasinet4addrstr(cchar *sp,int 탎l) noex {
 /* end subroutine (hasnet4addrstr) */
 
 bool hasinet6addrstr(cchar *sp,int 탎l) noex {
+    	cnullptr	np{} ;
 	bool		f = false ;
+	{
+	    strnul ds(sp,탎l) ;
+	    DPRINTF("ent s=>%s<\n",ccp(ds)) ;
+	}
 	if (int sl ; (sl = getlenstr(sp,탎l)) > 0) ylikely {
-	    int c = 0 ;
+	    int n = 0 ;
 	    int cz = 0 ;
-	    for (cc *tp ; (tp = strnchr(sp,sl,':')) != nullptr ; ) {
-		c += 1 ;
+	    int c = 0 ;
+	    int ndouble = 0 ;
+	    f = true ;
+	    for (cc *tp ; f && ((tp = strnchr(sp,sl,':')) != np) ; ) {
 		if (cint tl = intconv(tp - sp) ; tl > 0) {
+		    c += 1 ;
 		    f = hasINET6num(sp,tl) ;
 		    if (! f) break ;
 		} else {
+		    if (n > 0) {
+			ndouble += 1 ;
+		    }
 		    cz += 1 ;
 		} /* end if */
+		n += 1 ;
 	        sl -= intconv((tp + 1) - sp) ;
 	        sp = (tp + 1) ;
 	    } /* end for */
-	    if (f && (sl > 0)) {
+	    if (f) {
+	       if (sl > 0) {
 		    f = hasINET6num(sp,sl) ;
+		    c += 1 ;
+	       } else {
+		    cz += 1 ;
+	       } /* end if */
 	    } /* end if (remainder) */
-	    f = f && (c > 0) && (cz <= 1) ;;
+	    DPRINTF("check f=%u n=%d c=%d cz=%d\n",uint(f),n,c,cz) ;
+	    DPRINTF("check ndouble=%d\n",ndouble) ;
+	    if (f) {
+		f = f && ((n >= 2) && (n <= 7)) ;
+		f = f && (cz <= 2) ;
+		f = f && (ndouble <= 1) ;
+		f = f && !((ndouble >= 1) && (c > 6)) ;
+		f = f && !((ndouble == 0) && (c < 8)) ;
+		if (! f) {
+		    f = f || ((n == 2) && (c == 0)) ;
+	            f = f || ((c == 8) && (n == 7)) ;
+	            f = f || ((c >= 1) && (n == 2) && (cz == 2)) ;
+		}
+	    } /* end if (going) */
 	} /* end if (getlenstr) */
+	DPRINTF("ret f=%u\n",f) ;
 	return f ;
 } /* end subroutine (hasnet6addrstr) */
 
@@ -163,7 +196,7 @@ local bool hasINET4num(cchar *sp,int sl) noex {
 } /* end subroutine (hasINET4num) */
 
 local bool hasINET6num(cchar *sp,int sl) noex {
-    	bool f = true ;
+    	bool f = false ;
     	while (sl-- && sp[0]) {
 	    f = ishexlatin(sp[0]) ;
 	    if (! f) break ;
