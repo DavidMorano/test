b44f23bdb48ff2b2f3e6b00ff3360e9ce6c95d1a
David A.D. Morano <david.a.morano@gmail.com>
2026-03-14 00:43:26 -0400

    restore

---
+++ src/testbase64/nzeros.h
@@ -0,0 +1,32 @@
+/* nzeros HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* return the number of leading zeros in an integer */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-21, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	NZEROS_INCLUDE
+#define	NZEROS_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<stdintx.h>
+
+
+#endif /* NZEROS_INCLUDE */
+
+
