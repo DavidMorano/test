c560c7903aba54998d72997af29f7dbefe12928c
David A.D. Morano <david.a.morano@gmail.com>
2026-03-14 00:44:50 -0400

    restore

---
+++ src/testshortq/queue.h
@@ -0,0 +1,46 @@
+/* queue HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* vector operatins */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	QUEUE_INCLUDE
+#define	QUEUE_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+#include	<aiq.h>
+#include	<chariq.h>
+#include	<charq.h>
+#include	<ciq.h>
+#include	<cq.h>
+#include	<fifoelem.h>
+#include	<fifoitem.h>
+#include	<fifostr.h>
+#include	<intiq.h>
+#include	<piq.h>
+#include	<plainq.h>
+#include	<pq.h>
+#include	<q.h>
+#include	<shortq.h>
+#include	<slist.h>
+#include	<slq.h>
+
+
+#endif /* QUEUE_INCLUDE */
+
+
