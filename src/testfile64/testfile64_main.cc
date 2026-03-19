7256e3bbd43d15e14ffe2ed84ee4c3cacdbd9fe4
David A.D. Morano <david.a.morano@gmail.com>
2026-03-19 00:40:11 -0400

    restore

---
+++ src/testfile64/testfile64_main.cc
@@ -0,0 +1,80 @@
+/* testfile64_main SUPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
+
+/* test the |uc_truncate(3uc)| subroutine */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-13, David A-D- Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
+
+/*******************************************************************************
+
+  	Name:
+	testfile64
+
+	Description:
+	Test the |uc_truncate(3uc)| subroutine.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<cstddef>
+#include	<cstdlib>
+#include	<cstdio>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<ucfileop.h>
+#include	<localmisc.h>
+
+
+/* local defines */
+
+
+/* local namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int main(int,mainv,mainv) {
+    	int		ex = EXIT_SUCCESS ;
+	int		rs = SR_OK ;
+	{
+	    rs = uc_truncate("here",0z) ;
+	}
+	if ((ex == EXIT_SUCCESS) && (rs < 0)) {
+	    ex = EXIT_FAILURE ;
+	}
+	return ex ;
+} /* end subroutine (main) */
+
+
