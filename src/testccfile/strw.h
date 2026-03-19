cefc1ac2090d534ce65876610f92523804a06204
David A.D. Morano <david.a.morano@gmail.com>
2026-03-19 00:39:37 -0400

    restore

---
+++ src/testccfile/strw.h
@@ -0,0 +1,35 @@
+/* strw HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* string-copy variant */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	STRW_INCLUDE
+#define	STRW_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+#include	<strwcpy.h>
+#include	<strwcmp.h>
+#include	<strwset.h>
+
+
+#endif /* STRW_INCLUDE */
+
+
