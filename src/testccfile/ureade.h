bbc01bc49f46c58a7519013db6a8abb14be60d4b
David A.D. Morano <david.a.morano@gmail.com>
2026-03-19 00:40:24 -0400

    restore

---
+++ src/testccfile/ureade.h
@@ -0,0 +1,47 @@
+/* ureade HEADER (UNIX® file operations) */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* translation layer interface for UNIX® equivalents */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services (RNS).
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Names:
+	u_reade
+
+	Description:
+	All of the UNIX® system calls that use (operate on) a
+	file-descroptor.
+
+*******************************************************************************/
+
+#ifndef	UREADE_INCLUDE
+#define	UREADE_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int	u_reade(int,void *,int,int,int) noex ;
+
+EXTERNC_end
+
+
+#endif /* UREADE_INCLUDE */
+
+
