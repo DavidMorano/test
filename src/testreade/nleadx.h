1c8a2ccc571bf2e87f329d82bc22d852b1c85e6d
David A.D. Morano <david.a.morano@gmail.com>
2026-03-21 00:24:51 -0400

    restore

---
+++ src/testreade/nleadx.h
@@ -0,0 +1,34 @@
+/* nleadx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* find leading substrings */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	NLEADX_INCLUDE
+#define	NLEADX_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+#include	<nleadkeystr.h>
+#include	<nleadstr.h>
+
+
+#endif /* NLEADX_INCLUDE */
+
+
