d16d0aeef82cc9e400742371ae78a2e9f71b6d8f
David A.D. Morano <david.a.morano@gmail.com>
2026-03-21 00:22:26 -0400

    restore

---
+++ src/testccfile/fmtflag.h
@@ -0,0 +1,32 @@
+/* fmtflag HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* File-Once management */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	FMTFLAG_INCLUDE
+#define	FMTFLAG_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+
+#endif /* FMTFLAG_INCLUDE */
+
+
