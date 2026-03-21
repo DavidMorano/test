4191169f89e30f31c2e7ab4c4e8eaea075334f7f
David A.D. Morano <david.a.morano@gmail.com>
2026-03-21 00:22:49 -0400

    restore

---
+++ src/testccfile/funcodes.h
@@ -0,0 +1,166 @@
+/* funcodes HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* additional UNIX® limits support */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-21, David A­D­ Morano
+	This module was originally written.
+
+	= 2017-08-01, David A­D­ Morano
+	Updated for lack of interfaces in Apple-Darwin
+
+*/
+
+/* Copyright © 1998,2017 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Name:
+	ucfunctioncodes
+
+	Description:
+	Function codes for extended Input-Output operations.
+
+*******************************************************************************/
+
+#ifndef	FUNCODES_INCLUDE
+#define	FUNCODES_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<sys/types.h>
+#include	<unistd.h>
+#include	<fcntl.h>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+/* control function codes */
+
+#define	FC_MASK		0x0000000F	/* control function mask */
+
+#define	fc_mask		FC_MACH
+
+/* control functions */
+
+enum fcs {
+	fc_noop,
+	fc_setmode,
+	fc_getmode,
+	fc_reestablish,
+	fc_setrterms,
+	fc_getsid,
+	fc_getpgrp,
+	fc_setpgrp,
+	fc_getpop,
+	fc_setpop,
+	fc_overlast
+} ; /* end enum (fcs) */
+
+#define	FC_NOOP		fc_noop
+#define	FC_SETMODE	fc_setmode	/* set terminal mode */
+#define	FC_GETMODE	fc_getmode	/* retrieve current mode */
+#define	FC_REESTABLISH	fc_reestablish	/* terminal re-establish */
+#define	FC_SETRTERMS	fc_setrterms	/* set read terminators */
+#define	FC_GETSID	fc_getsid	/* get control-terminal session ID */
+#define	FC_GETPGRP	fc_getpgrp	/* get control-terminal PGRP-ID */
+#define	FC_SETPGRP	fc_setpgrp	/* set control-term PGID */
+#define	FC_GETPOP	fc_getpop	/* get terminal POP information */
+#define	FC_SETPOP	fc_setpop	/* set terminal POP information */
+
+/* function modifiers */
+#define	FM_CMDMASK	0x0F
+#define	FM_OPTMASK	0xFFFFFFF0
+#define	FM_MASK		FM_OPTMASK
+
+#define	fm_mask		FM_MASK
+
+/* function modifiers (|open| and |setmode|) */
+#define	FM_LARGEFILE	(1<<(0+4))
+#define	FM_QUEUE	(1<<(1+4))
+#define	FM_NOREPORT	(1<<(2+4))
+#define	FM_INT		(1<<(2+4))	/* set interrupt attention */
+#define	FM_KILL		(1<<(3+4))	/* set control-Y attention */
+#define	FM_READATT	(1<<(5+4))	/* read-attention */
+#define	FM_WRITEATT	(1<<(6+4))	/* write-attention */
+#define	FM_GETSTATE	(1<<(7+4))
+#define	FM_CLEARSTATE	(1<<(8+4))
+#define	FM_NOSIG	(1<<(9+4))	/* do not generate signals */
+#define	FM_NOSIGECHO	(1<<(10+4))	/* no echo of signals */
+#define	FM_NOFLOW	(1<<(11+4))	/* no output flow-control */
+#define	FM_NET		(1<<(14+4))	/* specify network operation */
+
+#define	fm_queue	FM_QUEUE	
+#define	fm_noreport	FM_NOREPORT	
+#define	fm_largefile	FM_LARGEFILE
+#define	fm_int		FM_INT
+#define	fm_kill		FM_KILL
+#define	fm_readatt	FM_READATT
+#define	fm_writeatt	FM_WRITEATT
+#define	fm_getstate	FM_GETSTATE
+#define	fm_clearstate	FM_CLEARSTATE
+#define	fm_nosig	FM_NOSIG
+#define	fm_nosigecho	FM_NOSIGECHO
+#define	fm_noflow	FM_NOFLOW	/* no output flow control */
+#define	fm_net		FM_NET
+
+/* function modifiers (|read|) */
+#define	FM_NONE		0x0000		/* no options specified */
+#define	FM_ECHO		0x0000		/* echo data */
+#define	FM_FILTER	0x0000		/* do canonical processing */
+#define	FM_NOECHO	(1<<(0+4))	/* no echo on input */
+#define	FM_NOTECHO	(1<<(1+4))	/* no terminator echo */
+#define	FM_NOFILTER	(1<<(2+4))	/* no filter on input */
+#define	FM_RAWIN	(1<<(3+4))	/* raw input (no control) */
+#define	FM_NONBLOCK	(1<<(4+4))	/* do not start if no data */
+#define	FM_TIMED	(1<<(5+4))	/* timed read */
+#define	FM_EXACT	(1<<(6+4))	/* exact transfer length */
+#define	FM_AGAIN	(1<<(7+4))	/* return AGAIN if no data */
+#define	FM_SLOWPOLL	(1<<(8+4))	/* perform slower polling */
+#define	FM_TIMEINT	(1<<(9+4))	/* set time-out interval */
+#define	FM_SETMSG	(1<<(10+4))	/* set messages on terminal (what?) */
+#define	FM_NOFILTECHO	(1<<(11+4))	/* do not echo filter operations */
+
+#define	fm_none		FM_NONE
+#define	fm_echo		FM_ECHO
+#define	fm_filter	FM_FILTER
+#define	fm_noecho	FM_NOECHO
+#define	fm_notecho	FM_NOTECHO
+#define	fm_nofilter	FM_NOFILTER
+#define	fm_rawin	FM_RAWIN
+#define	fm_nonblock	FM_NONBLOCK
+#define	fm_timed	FM_TIMED
+#define	fm_exact	FM_EXACT
+#define	fm_again	FM_AGAIN
+#define	fm_slowpoll	FM_SLOWPOLL
+#define	fm_timeint	FM_TIMEINT	/* set time-out interval */
+#define	fm_setmsg	FM_SETMSG	/* what is this? */
+#define	fm_nofiltecho	FM_NOFILTECHO	/* do not echo filter operations */
+
+/* function modifiers (|write|) */
+#define	FM_CCO		(1<<(0+4))	/* cancel output cancel */
+#define	FM_RAWOUT	(1<<(1+4))	/* raw output */
+
+#define	fm_cco		FM_CCO
+#define	fm_rawout	FM_RAWOUT
+
+/* function modifiers (|status|) */
+
+/* function modifiers (|cancel|) */
+#define	FM_CANALL	(1<<(0+4))	/* cancel all requests w/ FC */
+#define	FM_ALL		FM_CANALL	/* cancel all requests w/ FC */
+
+#define	fm_canall	FM_CANALL
+#define	fm_all		FM_ALL
+
+
+#endif /* FUNCODES_INCLUDE */
+
+
