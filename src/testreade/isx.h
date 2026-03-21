148f6259de822d433113eac5ab35974eaba5593d
David A.D. Morano <david.a.morano@gmail.com>
2026-03-21 00:25:18 -0400

    restore

---
+++ src/testreade/isx.h
@@ -0,0 +1,51 @@
+/* isx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* return a boolean on some condition */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1996-03-01, David A­D­ Morano
+	The subroutine set was written from scratch to do what the
+	previous program by the same name did.
+
+*/
+
+/* Copyright © 1996 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Facility:
+	isx
+
+	Description:
+	These subroutines are subroutine groups that all return
+	a boolean (truth-value) on some condition.
+
+*******************************************************************************/
+
+#ifndef	ISX_INCLUDE
+#define	ISX_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+#include	<ischarx.h>
+#include	<iserror.h>
+#include	<isindomain.h>
+#include	<isinetaddr.h>
+#include	<ismmclass.h>
+#include	<isnot.h>
+#include	<isoneof.h>
+
+
+#endif /* ISX_INCLUDE */
+
+
