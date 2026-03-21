de53128b350b395ebffaea6f10ba06daa472b20d
David A.D. Morano <david.a.morano@gmail.com>
2026-03-21 00:24:19 -0400

    restore

---
+++ src/testreade/rmx.h
@@ -0,0 +1,36 @@
+/* rmx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* remove EOL (End-Of-Line) characters from the given string */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	RMX_INCLUDE
+#define	RMX_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+#include	<rmx_chr.h>
+#include	<rmx_class.h>
+#include	<rmx_misc.h>
+#include	<rmleadzero.h>
+
+
+#endif /* RMX_INCLUDE */
+
+
