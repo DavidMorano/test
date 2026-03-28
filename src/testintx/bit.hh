f8a6dda9fe761975b3799c1a5ac4ac9739649fb8
David A.D. Morano <david.a.morano@gmail.com>
2026-03-28 00:02:10 -0400

    restore

---
+++ src/testintx/bit.hh
@@ -0,0 +1,43 @@
+/* bit HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
+
+/* memory allocation facility (for library use) */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-85, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	BIT_INCLUDE
+#define	BIT_INCLUDE
+#ifdef	__cplusplus /* C++ only! */
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<climits>		/* |CHAR_BIT| */
+#include	<cstddef>
+#include	<cstdlib>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+
+template<typename T>
+constexpr inline bool bit(T v,int n) noex {
+	return !!((v >> n) & 1) ;
+} /* end subroutine (bit) */
+
+
+#endif /* __cplusplus (C++ only) */
+#endif /* BIT_INCLUDE */
+
+
