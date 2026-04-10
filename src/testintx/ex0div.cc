36072533d30d77e62aa97b1a927dd237f4a0265c
David A.D. Morano <david.a.morano@gmail.com>
2026-04-10 00:08:31 -0400

    restore

--- src/testintx/ex0div.cc
+++ src/testintx/ex0div.cc
@@ -26,7 +26,7 @@ constexpr int	chlim = (UCHAR_MAX+1) ;
 
 
 #ifdef	COMMENT
-local int cmp(const uchar *a,const uchar *b,int n) {
+local int cmp(con uchar *a,con uchar *b,int n) {
     for (int i = n - 1 ; i >= 0 ; --i) {
         if (a[i] < b[i]) return -1;
         if (a[i] > b[i]) return +1;
@@ -40,23 +40,35 @@ local int cmp(const uchar *a,const uchar *b,int n) {
  * arrays are little-endian externally,
  * but comparison here is done high-byte first.
  */
-local int vcmp(int n,const uchar *a,const uchar *b) {
-    int res = 0 ;
+local int vcmp(int n,con uchar *a,con uchar *b) {
+    int rc = 0 ;
     for (int i = (n-1) ; i >= 0 ; --i) {
 	if (a[i] != b[i]) {
-	    res = (a[i] > b[i]) ? +1 : -1 ;
+	    rc = (a[i] > b[i]) ? +1 : -1 ;
 	    break ;
 	}
     } /* end for */
-    return res ;
+    return rc ;
 } /* end subroutine */
+
+template<typename Dig> constexpr void vclear(int n,mut Dig *v) noex {
+	for (int i = 0 ; i < n ; i += 1) {
+	    v[i] = 0 ;
+	}
+} /* end subroutine (vclear) */
+
+template<typename Dig> constexpr void vcopy(int n,mut Dig *r,con Dig *v) noex {
+	for (int i = 0 ; i < n ; i += 1) {
+	    r[i] = v[i] ;
+	}
+} /* end subroutine (vcopy) */
 
 /*
  * a := a - b   over n bytes
  * little-endian
  */
-local int vsub(int n,uchar *a,const uchar *b) {
-    int borrow = 0;
+local int vsub(int n,mut uchar *a,con uchar *b) {
+    int borrow = 0 ;
     for (int i = 0 ; i < n ; ++i) {
         int t = int(a[i]) - int(b[i]) - borrow ;
         if (t < 0) {
@@ -70,19 +82,33 @@ local int vsub(int n,uchar *a,const uchar *b) {
     return borrow ;
 } /* end subroutine (vsub) */
 
+/* how many non-zero digits are in a divisor-array, on the low end */
+export {
+    template<typename TA> constexpr int nsigdig(int n,con TA *d) noex {
+    	int c = 0 ;
+    	for (int i = (n - 1) ; i >= 0 ; --i) {
+	    if (d[i]) {
+		c = (i + 1) ;
+		break ;
+	    }
+	} /* end for */
+    	return c ;
+    } /* end subroutine-template (nsigdig) */
+} /* end export */
+
 /*
  * Multiply 4-byte divisor by one byte q
  * result = 5-byte product
  * little-endian
  */
-local void mul_byte(uchar *temp,const uchar *divisor,uchar q) {
+local void mul_byte(mut uchar *temp,con uchar *divisor,uchar q) {
     int carry = 0;
     for (int i = 0 ; i < 4 ; ++i) {
-        int t = divisor[i] * q + carry;
+        int t = divisor[i] * q + carry ;
         temp[i] = uchar(t & UCHAR_MAX) ;
-        carry = t >> 8;
+        carry = t >> CHAR_BIT ;
     } /* end for */
-    temp[4] = (uchar)carry;
+    temp[4] = uchar(carry) ;
 } /* end subroutine */
 
 /*
@@ -90,11 +116,11 @@ local void mul_byte(uchar *temp,const uchar *divisor,uchar q) {
  * multiply by 256
  * little-endian means move upward
  */
-local void shift_left(uchar *x,int n) {
+local void shift_left(mut uchar *x,int n) {
     for (int i = (n-1) ; i > 0 ; --i) {
-        x[i] = x[i-1];
+        x[i] = x[i-1] ;
     } /* end for */
-    x[0] = 0;
+    x[0] = 0 ;
 } /* end subroutine */
 
 /*
@@ -102,32 +128,39 @@ local void shift_left(uchar *x,int n) {
  * + all arrays little-endian
    + n= number-of-dividend (and quotient) digits
  */
-void div(int n,uchar *quotient,uchar *remainder,
-         const uchar *dividend,const uchar *divisor) {
+/*
+   template<typename T2,typename T1>
+*/
+n = 8 ;
+void vdiv(int n,mut uchar *quotient,mut uchar *remainder,
+         con uchar *dividend,con uchar *divisor) {
     cint	nb = nbits<uchar>
-    uchar rem[(n/2)+2];
-    uchar prod[(n/2)+2];
-    std::memset(quotient,0,n);
-    std::memset(rem,0,((n/2)+2));
+    cint	nh = (n/2) ;
+    uchar rem[nh+2] ;
+    uchar prod[nh+2] ;
+    vclear(n,quotient) ;
+    vcopy(nh,rem,dividend) ;
+    rem[nh+0] = 0 ;
+    rem[nh+1] = 0 ;
     /* * Process from most-significant dividend byte downward */
     for (int i = (n-1) ; i >= 0 ; --i) {
-        shift_left(rem,n);
-        rem[0] = dividend[i];
+        shift_left(rem,(nh+1)) ;
+        rem[nh-1] = dividend[i] ;
         /* * Estimate quotient digit from top two bytes */
-        ushort top = (ushort(rem[4]) << CHAR_BIT) | rem[3];
-        uchar q = uchar(top / divisor[3]);
+        ushort top = (ushort(rem[nh]) << nb) | rem[nh-1] ;
+        uchar q = uchar(top / divisor[nh-1]) ;
         /* * Correct estimate downward if needed */
         forever {
-            mul_byte(prod,divisor,q);
-            if (vcmp(((n/2)+1),rem,prod) >= 0) break;
+            mul_byte(prod,divisor,q) ;
+            if (vcmp((nh+1),rem,prod) >= 0) break ;
             q -= 1 ;
         } /* end forever */
-        quotient[i] = q;
+        quotient[i] = q ;
         /* mul_byte(prod,divisor,q); */
-        vsub(((n/2)+1),rem,prod) ;
+        vsub((nh+1),rem,prod) ;
     } /* end for */
     /* * final remainder: low 4 bytes */
-    for (int i = 0 ; i < (n/2) ; ++i) {
+    for (int i = 0 ; i < nh ; ++i) {
         remainder[i] = rem[i] ;
     }
 } /* end subroutine (div) */
