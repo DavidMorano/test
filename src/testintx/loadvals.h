46a4bbd15534a3bcfae5385365685e6c6ab1d8b5
David A.D. Morano <david.a.morano@gmail.com>
2026-04-13 01:04:53 -0400

    restore

---
+++ src/testintx/loadvals.h
@@ -0,0 +1,31 @@
+/* loadvals HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* subroutine interfaces to compiler built-in instrinsics */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	LOADVALS_INCLUDE
+#define	LOADVALS_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+#endif /* LOADVALS_INCLUDE */
+
+
