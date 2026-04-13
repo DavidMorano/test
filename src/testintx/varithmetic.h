d4430c28c01b28cc2b305b29ec6039655e8e12e7
David A.D. Morano <david.a.morano@gmail.com>
2026-04-13 01:04:57 -0400

    restore

---
+++ src/testintx/varithmetic.h
@@ -0,0 +1,31 @@
+/* varithmetic HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* subroutine interfaces to compiler built-in instrinsics */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	VARITHMETIC_INCLUDE
+#define	VARITHMETIC_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+#endif /* VARITHMETIC_INCLUDE */
+
+
