38916ab5cdde7ab642a2f17682334cbe426d3e32
David A.D. Morano <david.a.morano@gmail.com>
2026-04-18 00:10:31 -0400

    update

--- src/testmisc/testdiv.cc
+++ src/testmisc/testdiv.cc
@@ -25,20 +25,16 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<cstring>
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<cstdio>
 #include	<cmath>
-#include	<iostream>		/* |cout(3c++)| */
 #include	<clanguage.h>
 #include	<usysbase.h>
 #include	<usupport.h>
-#include	<mkchar.h>
 #include	<localmisc.h>
 
 using libu::cfdec ;			/* subroutine */
-using std::cout ;			/* variable */
 
 int main(int argc,mainv argv,mainv) {
     	int		ex = EXIT_SUCCESS ;
@@ -47,13 +43,15 @@ int main(int argc,mainv argv,mainv) {
 	    for (int i = 1 ; i < argc ; ++i) {
 		cchar *ap = argv[i] ;
 		if (*ap) {
-	            ulong	num ;
-		    if ((rs = cfdec(ap,-1,&num)) >= 0) {
+	            if (ulong num ; (rs = cfdec(ap,-1,&num)) >= 0) {
 	                ulong	den = argc ;
     	                ulong quo ;
 	                int rem ;
 	                quo = num / den ;
 	                rem = int(num % den) ;
+			printf("-\n") ;
+	                printf("dividend=%lu\n",num) ;
+	                printf("divisor=%lu\n",den) ;
 	                printf("quo=%lu\n",quo) ;
 	                printf("rem=%u\n",rem) ;
 		    } /* end if (cfdec) */
