a465a229a9aa0dd8f8130a79e4746384d25ddd1e
David A.D. Morano <david.a.morano@gmail.com>
2026-04-24 00:08:05 -0400

    update

--- src/testmisc/testaf.cc
+++ src/testmisc/testaf.cc
@@ -1,9 +1,30 @@
+/* testaf SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
+
+/* test the 'usysdefs_f.h" header file */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-13, David A-D- Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2017 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
+
 
 #include	<envstandards.h>
+#include	<cstddef>
+#include	<cstdlib>
 #include	<cstdio>
 #include	<usysdefs_af.h>
 
 int main() {
     printf("Hello world!\n") ;
 }
+
 
