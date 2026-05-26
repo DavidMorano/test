d9796a234f59cf7485221b7a52c8a25bc8a90137
David A.D. Morano <david.a.morano@gmail.com>
2026-05-26 00:52:02 -0400

    update

--- src/testvmail/testhdrdecode.cc
+++ src/testvmail/testhdrdecode.cc
@@ -25,7 +25,7 @@
 *******************************************************************************/
 
 
-#include	<envstandards.h>
+#include	<envstandards.h>	/* ordered first to configure */
 
 #include	<sys/types.h>
 #include	<climits>
