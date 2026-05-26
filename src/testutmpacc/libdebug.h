b871e00e1e0ed58ca2b7762d21d73637508a24e2
David A.D. Morano <david.a.morano@gmail.com>
2026-05-26 00:50:42 -0400

    restore

---
+++ src/testutmpacc/libdebug.h
@@ -0,0 +1,39 @@
+/* libdebug HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
+
+/* debug utilities */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-02-01, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	LIBDEBUG_INCLUDE
+#define	LIBDEBUG_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<strlinelen.h>
+
+#include	<debugprime.h>
+#include	<debugmkhex.h>
+#include	<debugprint.h>
+#include	<debugline.h>
+#include	<debugutil.hh>		/* C++ only */
+
+#include	<nprintf.h>
+#include	<zprintf.h>
+
+
+#endif /* LIBDEBUG_INCLUDE */
+
+
