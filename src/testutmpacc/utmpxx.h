aa36111f1ee38bad88868846ac8e785ce37a97b8
David A.D. Morano <david.a.morano@gmail.com>
2026-05-26 00:51:08 -0400

    restore

---
+++ src/testutmpacc/utmpxx.h
@@ -0,0 +1,35 @@
+/* utmpxx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* convert a single character to a given case */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-02-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	UTMPXX_INCLUDE
+#define	UTMPXX_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+#include	<utmptypes.hh>
+#include	<utmpsizes.hh>
+#include	<utmpacc.h>
+#include	<utmpaccent.h>
+#include	<utmpent.h>
+
+
+#endif /* UTMPXX_INCLUDE */
+
+
