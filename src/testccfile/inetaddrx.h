ed4939d05b0696bc936ee9800ce4ab884d763414
David A.D. Morano <david.a.morano@gmail.com>
2026-05-27 00:40:49 -0400

    restore

---
+++ src/testccfile/inetaddrx.h
@@ -0,0 +1,35 @@
+/* inetaddrx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* convert a single character to a given case */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	INETADDRX_INCLUDE
+#define	INETADDRX_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+#include	<inetaddr.h>
+#include	<inetaddrparse.h>
+#include	<inetconv.h>
+#include	<inet4int.h>
+#include	<inaddrbad.hh>
+
+
+#endif /* INETADDRX_INCLUDE */
+
+
