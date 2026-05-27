8bb8e3b841cede164d85e25d768ae6b729b8c873
David A.D. Morano <david.a.morano@gmail.com>
2026-05-27 00:40:57 -0400

    restore

---
+++ src/testccfile/strcpyx.h
@@ -0,0 +1,33 @@
+/* strcpyx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* copy a c-string to destination buffer w/ variations on the theme */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	STRCPYX_INCLUDE
+#define	STRCPYX_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+#include	<strcpyxc.h>
+
+
+#endif /* STRCPYX_INCLUDE */
+
+
