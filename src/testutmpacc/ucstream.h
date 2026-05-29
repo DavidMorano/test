674ae4b29372a10ba031874f2538b0a09709ac7d
David A.D. Morano <david.a.morano@gmail.com>
2026-05-29 00:36:34 -0400

    restore

---
+++ src/testutmpacc/ucstream.h
@@ -0,0 +1,29 @@
+/* ucstream HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* file stream operations for LIBUC */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	UCSTREAM_INCLUDE
+#define	UCSTREAM_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+#endif /* UCSTREAM_INCLUDE */
+
+
