d55435831928b984d4c2039f6ef3e350384e1ea2
David A.D. Morano <david.a.morano@gmail.com>
2026-05-29 00:36:54 -0400

    restore

---
+++ src/testutmpacc/openx.h
@@ -0,0 +1,32 @@
+/* openx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* opening of special files */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2000-05-14, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services
+	(RNS).
+
+*/
+
+/* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	OPENX_INCLUDE
+#define	OPENX_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+#include	<opentmp.h>
+
+
+#endif /* OPENX_INCLUDE */
+
+
