366b1b02cb8c021845139bd585990676bf10e7e3
David A.D. Morano <david.a.morano@gmail.com>
2026-06-04 00:33:47 -0400

    restore

---
+++ src/testenvmk/getxname.h
@@ -0,0 +1,41 @@
+/* getxname HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* get various (usually system related) names */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	GETXNAME_INCLUDE
+#define	GETXNAME_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+#include	<getchostname.h>
+#include	<getclustername.h>
+#include	<getexecname.h>
+#include	<getfname.h>
+#include	<getgroupname.h>
+#include	<getnodename.h>
+#include	<getournetname.h>
+#include	<getprojname.h>
+#include	<getrealname.h>
+#include	<getsysname.h>
+#include	<getusername.h>
+
+
+#endif /* GETXNAME_INCLUDE */
+
+
