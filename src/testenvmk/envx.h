212c8552c517917d8b5289d64a2bc60ba2a240e1
David A.D. Morano <david.a.morano@gmail.com>
2026-06-04 00:33:37 -0400

    restore

---
+++ src/testenvmk/envx.h
@@ -0,0 +1,34 @@
+/* envx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* Environment Manager */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-01-14, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	RNVX_INCLUDE
+#define	RNVX_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be ordered first to configure */
+#include	<clanguage.h>
+#include	<usyscalls.h>		/* |ulogerror(3u)| */
+
+#include	<envhelp.h>
+#include	<envlist.h>
+#include	<envmgr.h>
+#include	<envmk.h>
+
+
+#endif /* RNVX_INCLUDE */
+
+
