67b13715610dfb8eeaab6c24df726a6c1c4e4116
David A.D. Morano <david.a.morano@gmail.com>
2026-06-15 01:30:48 -0400

    update

--- src/testuctim/testuctim_main.cc
+++ src/testuctim/testuctim_main.cc
@@ -16,15 +16,13 @@
 /* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<cstddef>
-#include	<cstdlib>
-#include	<cstdio>
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<localmisc.h>
-#include	<libdebug.h>		/* LIBDEBUG */
-
-#include	"hasprint.h"
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<cstdio>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
+#include	<libdebug.h>		/* LIBDEBUG |DEUGPRINTF(3debug)| */
 
 
 /* local defines */
