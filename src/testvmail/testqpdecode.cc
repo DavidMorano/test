d038f72da8d734f1b8e696db0b45fedd5165e72c
David A.D. Morano <david.a.morano@gmail.com>
2026-07-03 00:17:18 -0400

    update

--- src/testvmail/testqpdecode.cc
+++ src/testvmail/testqpdecode.cc
@@ -16,7 +16,7 @@
 /* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
 
 
-#include	<envstandards.h>
+#include	<envstandards.h>	/* ordered first to configure */
 
 #include	<sys/types.h>
 #include	<climits>
