25a3a901030ec47acbba3c5101b3bd8d7bd08549
David A.D. Morano <david.a.morano@gmail.com>
2026-07-09 00:23:13 -0400

    update

--- src/testloadave/loadave_main.cc
+++ src/testloadave/loadave_main.cc
@@ -2,16 +2,16 @@
 /* charset=ISO8859-1 */
 /* version %I% last-modified %G% */
 
-/* test out Sun Solaris UNIX® 'kstat's */
+/* test the LIBUC |loadave(3uc)| object */
 /* version %I% last-modified %G% */
 
-#define	CF_DEBUG	1
+#define	CF_DEBUG	1		/* debugging */
 
 /* revision history:
 
 	= 1988-01-10, David A­D­ Morano
 	This subroutine was written (originally) as a test of
-	the Sun Solaris UNIX 'kstat' facility.
+	the LIBUC |loadave(3uc)| object.
 
 */
 
@@ -19,56 +19,43 @@
 
 /*******************************************************************************
 
-	Test the Sun Solaris® UNIX 'kstat' facility.
+  	Description:
+	Test the LOADAVE object.
 
 *******************************************************************************/
 
 #include	<envstandards.h>	/* must be ordered first to configure */
-#include	<sys/types.h>
-#include	<sys/param.h>
-#include	<sys/mman.h>
-#include	<sys/systeminfo.h>
-#include	<sys/socket.h>
-#include	<netinet/in.h>
-#include	<unistd.h>
-#include	<fcntl.h>
-#include	<termios.h>
-#include	<csignal>
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<cstring>
-#include	<kstat.h>
-#include	<usystem.h>
-#include	<field.h>
-#include	<bfile.h>
-#include	<exitcodes.h>
-#include	<localmisc.h>
-
-#include	"config.h"
-#include	"defs.h"
-#include	"loadave.h"
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<cstring>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<ucmem.h>		/* LIBU */
+#include	<ucsysmisc.h>		/* LIBU */
+#include	<ucnprocs.h>		/* LIBU */
+#include	<loadave.h>		/* LIBUC */
+#include	<timestr.h>		/* LIBUC */
+#include	<exitcodes.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
+#include	<libdebug.h>		/* LIBDEBUG |DEBUGPRINTF(3debug)| */
 
 
 /* local defines */
 
 #define	TO		11
 
-#ifndef	BUFLEN
-#define	BUFLEN		(MAXPATHLEN * 2)
-#endif
-
-#ifndef	TIMEBUFLEN
-#define	TIMEBUFLEN	80
-#endif
-
 #define	LOOPS		10
+
+#ifndef	CF_DEBUG
+#define	CF_DEBUG	1		/* debugging */
+#endif
+
+#define	LA		loadave
+#define	LA_MID		loadave_mid
+#define	LA_VAL		loadave_val
 
 
 /* external subroutines */
-
-extern int	sfbasename(const char *,int,const char **) ;
-
-extern char	*timestr_log(time_t,char *) ;
 
 
 /* external variables */
@@ -76,16 +63,12 @@ extern char	*timestr_log(time_t,char *) ;
 
 /* forward references */
 
-static void	int_alarm() ;
-static void	int_signal() ;
-
-static int	bprintla() ;
+local int	printla(uint v,cchar *s) noex ;
 
 
 /* local variables */
 
-static volatile int	f_alarm ;
-static volatile int	f_signal ;
+cbool		f_debug		= CF_DEBUG ;
 
 
 /* exported variables */
@@ -93,182 +76,58 @@ static volatile int	f_signal ;
 
 /* exported subroutines */
 
-int main(int argc,mainv argv,mainv)
-	struct sigaction	sigs ;
-	LOADAVE			la ;
-	LOADAVE_VALUES		lav ;
-	LOADAVE_MID		mid ;
-	bfile		input, *ifp = &input ;
-	bfile		output, *ofp = &output ;
-	bfile		error, *efp = &error ;
-	sigset_t	signalmask ;
-	kstat_ctl_t	*kcp ;
-	kstat_t		*ksp = NULL ;
-	kid_t		kid ;
-	ulong		hostid = gethostid() ;
-	int		rs = SR_OK ;
-	int		maxmsglen, trylen, len, conlen ;
-	int		i, j ;
-	int		sl ;
-	int		s ;
-	int		fd = -1 ;
-	int		err_fd = -1 ;
-	const char	*progname ;
-	cchar		*cp ;
-	char		buf[BUFLEN + 1], buf2[BUFLEN + 1], *bp ;
-	char		timebuf[TIMEBUFLEN + 1] ;
-
-
-	if (((cp = getenv("ERROR_FD")) != NULL) &&
-	    (cfdeci(cp,-1,&err_fd) >= 0))
-	    debugsetfd(err_fd) ;
-
-
-	sfbasename(argv[0],-1,&progname) ;
-
-	if (bopen(efp,BFILE_STDERR,"dwca",0664) >= 0)
-	    bcontrol(efp,BC_LINEBUF,0) ;
-
-	bopen(ofp,BFILE_STDOUT,"dwct",0666) ;
-
-
-/* print out some common accessible machine parameters */
-
-	sl = u_sysinfo(SI_HW_PROVIDER,buf,BUFLEN) ;
-
-#if	CF_DEBUGS
-	debugprintf("main: hw_provider rs=%d\n",sl) ;
-#endif
-
-	bprintf(ofp,"provider> %W\n",buf,sl) ;
-
-
-	sl = u_sysinfo(SI_HW_SERIAL,buf,BUFLEN) ;
-
-	bprintf(ofp,"serial> %W\n",buf,sl) ;
-
-
-#if	CF_DEBUGS
-	debugprintf("main: hostid rs=%d\n",rs) ;
-#endif
-
-	bprintf(ofp,"hostid> %08x (%u)\n",
-	    (uint) hostid,
-	    (uint) hostid) ;
-
-
-
-#if	CF_DEBUGS
-	debugprintf("main: about to open the KSTATs\n") ;
-#endif
-
-	if ((rs = loadave_start(&la)) >= 0) {
-
-#if	CF_DEBUGS
-	    debugprintf("main: loadave_start() rs=%d\n",rs) ;
-#endif
-
-	    rs = loadave_readmid(&la,&mid) ;
-
-	    bprintf(ofp,"mid_time> %s\n",
-	        timestr_log(mid.tim_read,timebuf)) ;
-
-	    bprintf(ofp,"provider> %s\n",mid.provider) ;
-
-	    bprintf(ofp,"serial> %s\n",mid.serial) ;
-
-
-	    rs = loadave_readvalues(&la,&lav) ;
-
-	    for (i = 0 ; (rs >= 0) && (i < LOOPS) ; i += 1) {
-
-	        if (i != 0) {
-
-	            sleep(5) ;
-
-	            rs = loadave_readvalues(&la,&lav) ;
-
-	        }
-
-#if	CF_DEBUGS
-	        debugprintf("main: loadave_read() rs=%d\n",rs) ;
-#endif
-
-	        if (rs >= 0) {
-
-	            bprintf(ofp,"la_time> %s\n",
-	                timestr_log(lav.tim_read,timebuf)) ;
-
-	            bprintf(ofp,"nproc=%d\n",lav.nproc) ;
-
-	            bprintf(ofp,"ncpu=%d\n",lav.ncpu) ;
-
-	            bprintla(ofp,lav.la1min,"la1min") ;
-
-	            bprintla(ofp,lav.la5min,"la5min") ;
-
-	            bprintla(ofp,lav.la15min,"la15min") ;
-
-	        }
-
-	    } /* end for */
-
-	    loadave_finish(&la) ;
-	} /* end if (opened) */
-
-
-	bclose(ofp) ;
-
-/* finished */
-done:
-	bclose(efp) ;
-
-	return EX_OK ;
-}
-/* end subroutine (main) */
-
-
-static void int_alarm(sig)
-int	sig ;
-{
-
-	f_alarm = TRUE ;
-}
-
-
-static void int_signal(sig)
-int	sig ;
-{
-
-	f_signal = TRUE ;
-}
-
+int main(int,con mainv,con mainv) {
+	int		rs ;
+	int		rs1 ;
+	int		ex = EXIT_SUCCESS ;
+	if (loadave la ; (rs = loadave_start(&la)) >= 0) {
+	    loadave_mid	mid ;
+	    if ((rs = loadave_readmid(&la,&mid)) >= 0) {
+		char timebuf[TIMEBUFLEN + 1] ;
+	        timestr_log(mid.tim_read,timebuf) ;
+	        printf("mid_time> %s\n",timebuf) ;
+	        printf("provider> %s\n",mid.provider) ;
+	        printf("serial>   %s\n",mid.serial) ;
+		loadave_val lav ;
+	        if ((rs = loadave_readval(&la,&lav)) >= 0) {
+	    	    for (int i = 0 ; (rs >= 0) && (i < LOOPS) ; i += 1) {
+	                if (i > 0) {
+	                    sleep(1) ;
+	                    rs = loadave_readval(&la,&lav) ;
+	                } /* end if */
+	                DEBUGPRINTF("loadave_read() rs=%d\n",rs) ;
+	                if (rs >= 0) {
+	                    timestr_log(lav.tim_la,timebuf) ;
+	                    printf("la_time> %s\n",timebuf) ;
+	                    printf("nproc=%d\n",lav.nprocs) ;
+	                    printf("ncpu=%d\n",lav.ncpu) ;
+	                    printla(lav.la1min,"la1min") ;
+	                    printla(lav.la5min,"la5min") ;
+	                    printla(lav.la15min,"la15min") ;
+	                } /* end if (ok) */
+	            } /* end for */
+	        } /* end if (loadave_readval) */
+	    } /* end if (loadave_readmid) */
+	    rs1 = loadave_finish(&la) ;
+	    if (rs >= 0) rs = rs1 ;
+	} /* end if (loadave) */
+	if ((ex == EXIT_SUCCESS) && (rs < 0)) {
+	    ex = EXIT_FAILURE ;
+	}
+	return ex ;
+} /* end subroutine (main) */
 
 
 /* local subroutines */
 
-
-
-static int bprintla(ofp,v,s)
-bfile		*ofp ;
-uint		v ;
-char		s[] ;
-{
-	int	la_i, la_f ;
-	int	la ;
-	int	rs = BAD ;
-
-
-	bprintf(ofp,"%-14s",s) ;
-
-	bprintf(ofp," ul=%lu",v) ;
-
-	la_i = v / FSCALE ;
-	la_f = +((v % FSCALE) * 1000) / 256 ;
-
-	bprintf(ofp," ula=%d.%03d\n",la_i,la_f) ;
-
+local int printla(uint v,cchar *s) noex {
+	uint	la_i = (v >> FSHIFT) ;
+	uint	la_f = +((v % FSCALE) * 1000) / FSCALE ;
+	int	rs = SR_OK ;
+	printf("%-14s",s) ;
+	printf(" l=%u",v) ;
+	printf(" la=%u.%04u\n",la_i,la_f) ;
 	return rs ;
-} /* end subroutine (bprintla) */
+} /* end subroutine (printla) */
 
 
