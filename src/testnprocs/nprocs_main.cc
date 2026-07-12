ed6ab99d84f88c220549191312eab7717df11dbc
David A.D. Morano <david.a.morano@gmail.com>
2026-07-12 00:22:22 -0400

    update

--- src/testnprocs/nprocs_main.cc
+++ src/testnprocs/nprocs_main.cc
@@ -24,6 +24,7 @@
 #include	<cstdio>		/* CSTD */
 #include	<clanguage.h>		/* LIBU */
 #include	<usysbase.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
 #include	<usys.h>		/* LIBU */
 #include	<localmisc.h>		/* LIBU */
 
@@ -31,17 +32,54 @@
 /* local defines */
 
 
+/* imported namespaces */
+
+using libu::matstr ;			/* subroutine */
+
+
+/* local typedefs */
+
+
+/* local variables */
+
+constexpr cpcchar	names[] = {
+    	"all",
+	"system",
+	"user",
+	"session",
+	nullptr
+} ; /* end arrat */
+
+
 /* exported variables */
 
 
 /* exported subroutines */
 
-int main(int,con mainv,con mainv) {
+local int lister(int w) noex {
+    	int	rs ;
+	if ((rs = usys::usys_nprocs(w)) >= 0) {
+	    printf("nprocs=%d\n",rs) ;
+	}
+	return rs ;
+} /* end subroutine (lister) */
+
+int main(int argc,con mainv argv,con mainv) {
 	int		rs = SR_OK ;
 	int		ex = EXIT_SUCCESS ;
-	if ((rs = usys_nprocs()) >= 0) {
-	    printf("nprocs=%d\n",rs) ;
+	int		w = 0 ;
+	if (argc > 1) {
+	    for (int ai = 1 ; ai < argc ; ai += 1) {
+	        if (cchar *ap = argv[ai] ; ap[0]) {
+	    	    if (cint mi = matstr(names,ap) ; mi >= 0) {
+			rs = lister(mi) ;
+		    } /* end if (matstr) */
+		} /* end if (non-empty) */
+	    } /* end for */
 	} else {
+	    rs = lister(w) ;
+	} /* end if */
+	if ((ex == EXIT_SUCCESS) && (rs < 0)) {
 	    printf("error rs=%d\n",rs) ;
 	    ex = EXIT_FAILURE ;
 	}
