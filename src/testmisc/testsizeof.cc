65def08882dc857d2906f7086d76ee030e094eb4
David A.D. Morano <david.a.morano@gmail.com>
2026-07-22 00:26:35 -0400

    update

--- src/testmisc/testsizeof.cc
+++ src/testmisc/testsizeof.cc
@@ -2,7 +2,6 @@
 /* charset=ISO8859-1 */
 /* lang=C99 */
 
-
 /* test getting the size of various types (using 'sizeof') */
 /* version %I% last-modified %G% */
 
@@ -12,24 +11,21 @@
 	= 2000-05-14, David A­D­ Morano
 	Originally written for Rightcore Network Services.
 
-	= 2017-10-06, David A­D­ Morano
-	Updated and enhanced.
-
 */
 
-/* Copyright © 2000,2017 David A­D­ Morano.  All rights reserved. */
+/* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
 
 #include	<envstandards.h>	/* MUST be ordered first to configure */
-#include	<sys/param.h>
-#include	<sys/stat.h>
-#include	<ctime>
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>		/* |getenv(3c)| */
-#include	<cstdarg>
-#include	<cstdio>
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<localmisc.h>
+#include	<sys/param.h>		/* POSIX */
+#include	<sys/stat.h>		/* POSIX */
+#include	<ctime>			/* CSTD */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<cstdarg>		/* CSTD */
+#include	<cstdio>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
 
 
 /* local defines */
@@ -40,13 +36,14 @@
 
 /* exported subroutines */
 
-int main(int,mainv,mainv) {
+int main(int,con mainv,con mainv) {
 	int		rs = SR_OK ;
 	int		rs1 ;
 	int		ex = EXIT_SUCCESS ;
-
-	printf("nlink_t=%d\n",szof(nlink_t)) ;
-
+	{
+	    csize sz = sizeof(nlink_t) ;
+	    printf("nlink_t=%lu\n",sz) ;
+	}
 	return ex ;
 } /* end subroutine (main) */
 
