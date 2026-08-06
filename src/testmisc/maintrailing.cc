21e2a4276351aa61832f4e50fc1cd665acc9d387
David A.D. Morano <david.a.morano@gmail.com>
2026-08-06 00:54:38 -0400

    update

--- src/testmisc/maintrailing.cc
+++ src/testmisc/maintrailing.cc
@@ -30,7 +30,7 @@
 
 *******************************************************************************/
 
-#include	<envstandards.h>
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<sys/types.h>
 #include	<cstdlib>
 #include	<cinttypes>
