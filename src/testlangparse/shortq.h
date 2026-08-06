548ec270339eb5d9b21ee1dfcb910f5f8ec32e88
David A.D. Morano <david.a.morano@gmail.com>
2026-08-06 00:49:52 -0400

    update

--- src/testlangparse/shortq.h
+++ src/testlangparse/shortq.h
@@ -20,8 +20,8 @@
 
 
 #include	<envstandards.h>	/* MUST be ordered first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 
 #define	SHORTQ		struct shortq_head
