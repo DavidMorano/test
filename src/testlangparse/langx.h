cd312f364c6c47008ec52612aeb70a91f2411c3f
David A.D. Morano <david.a.morano@gmail.com>
2026-08-06 00:49:30 -0400

    update

--- src/testlangparse/langx.h
+++ src/testlangparse/langx.h
@@ -20,11 +20,8 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 #include	<langstate.h>
 #include	<langproc.h>
