59fdd6c7943724c395b3e0268a3af7384e2814ed
David A.D. Morano <david.a.morano@gmail.com>
2026-08-07 00:52:00 -0400

    update

--- src/testreade/six.h
+++ src/testreade/six.h
@@ -20,8 +20,8 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 #include	<sicasechr.h>
 #include	<sichar.h>
