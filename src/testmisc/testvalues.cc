80852171030012d0cb65a0c4f08fcb4cc1a192cf
David A.D. Morano <david.a.morano@gmail.com>
2026-08-07 00:49:16 -0400

    update

--- src/testmisc/testvalues.cc
+++ src/testmisc/testvalues.cc
@@ -2,7 +2,7 @@
 /* charset=ISO8859-1 */
 /* lang=C++20 (conformance reviewed) */
 
-/* test getting some operating system values */
+/* test how the more recent GCC compiler versions handle 'volatile' */
 /* version %I% last-modified %G% */
 
 
@@ -19,8 +19,8 @@
 /*******************************************************************************
 
   	Description:
-	This is test code (in the present case mostly the |getconf(2)|
-	system call).
+	This is code that explores how the more recent GCC compiler
+	versions handle the 'volatile' feature.
 
 *******************************************************************************/
 
@@ -28,51 +28,42 @@
 #include	<sys/types.h>		/* POSIX® |getgroups(2)| */
 #include	<sys/param.h>		/* POSIX® |getgroups(2)| */
 #include	<unistd.h>		/* POSIX® |sysconf(2)| */
-#include	<cstddef>		/* CSTD |nullptr_t| */
+#include	<cstddef>		/* CSTD */
 #include	<cstdlib>		/* CSTD */
 #include	<cstdio>		/* CSTD */
 #include	<clanguage.h>		/* LIBU */
-#include	<utypedefs.h>		/* LIBU */
-#include	<utypealiases.h>	/* LIBU */
-#include	<usysdefs.h>		/* LIBU */
-#include	<usysrets.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 #include	<usyscalls.h>		/* LIBU */
+#include	<exitcodes.h>		/* LIBU */
 #include	<localmisc.h>		/* LIBU */
 
 
-local void	clktck() noex ;
-local void	maxgroups() noex ;
-local void	getgroups() noex ;
+local int testvol() noex ;
+
+volatile int	volval = 0 ;
 
 int main(int,con mainv,con mainv) {
-    	clktck() ;
-	maxgroups() ;
-	getgroups() ;
+    	int		rs = SR_OK ;
+	int		ex = EX_SUCCESS ;
+	if (rs >= 0) {
+    	    rs = testvol() ;
+	}
+	if ((rs ==  EXIT_SUCCESS) && (rs < 0)) {
+	    ex = EXIT_FAILURE ;
+	}
+	return ex ;
 } /* end subroutine (main) */
 
 
 /* local subroutines */
 
-local void clktck() noex {
-    	clong lv = sysconf(_SC_CLK_TCK) ;
+local int testvol() noex {
+    	int		rs = SR_OK ;
 	{
-	    cint	v = int(lv) ;
-	    printf("clk_tck=%d\n",v) ;
-	}
-} /* end subroutine (clktck) */
-
-local void maxgroups() noex {
-    	clong lv = sysconf(_SC_NGROUPS_MAX) ;
-	{
-	    cint	v = int(lv) ;
-	    printf("maxgroup=%d\n",v) ;
-	}
-} /* end subroutine (maxgoups) */
-
-local void getgroups() noex {
-    	gid_t	gids[256+1] ;
-    	cint	ng = getgroups(0,gids) ;
-	printf("getgroup=%d\n",ng) ;
-} /* end subroutine (getgoups) */
+	    cint v = ++volval ;
+	    printf("volval=%d\n",v) ;
+	} /* end block */
+    	return rs ;
+} /* end subroutine (testvol) */
 
 
