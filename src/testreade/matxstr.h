96017d16d3d3448770eb9bbb580bbacf8e029604
David A.D. Morano <david.a.morano@gmail.com>
2026-08-07 00:51:45 -0400

    update

--- src/testreade/matxstr.h
+++ src/testreade/matxstr.h
@@ -20,10 +20,10 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
 
 #include	<matnstr.h>
 #include	<matostr.h>
