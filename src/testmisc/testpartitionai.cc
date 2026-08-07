2d8f1561bd769f571b3f32cdb410ebd6358b4d4b
David A.D. Morano <david.a.morano@gmail.com>
2026-08-07 00:47:36 -0400

    update

--- src/testmisc/testpartitionai.cc
+++ src/testmisc/testpartitionai.cc
@@ -50,7 +50,7 @@
 *******************************************************************************/
 
 
-#include	<envstandards.h>
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<sys/types.h>
 #include	<climits>
 #include	<cstdlib>
