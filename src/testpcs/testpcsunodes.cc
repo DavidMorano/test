f89ae21f623fdcbc999062bd54916f13bd6e0782
David A.D. Morano <david.a.morano@gmail.com>
2026-08-07 00:50:34 -0400

    update

--- src/testpcs/testpcsunodes.cc
+++ src/testpcs/testpcsunodes.cc
@@ -12,7 +12,7 @@
 
 #define	CF_DEBUGS	1		/* compile-time debugging */
 #define	CF_DEBUGMALL	1		/* debug memory-allocations */
-#include	<envstandards.h>
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<sys/types.h>
 #include	<sys/param.h>
 #include	<usystem.h>
