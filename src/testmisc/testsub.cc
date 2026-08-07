d72dfb8be8352be90b02fe28a3b1f093786d6d6c
David A.D. Morano <david.a.morano@gmail.com>
2026-08-07 00:49:03 -0400

    update

--- src/testmisc/testsub.cc
+++ src/testmisc/testsub.cc
@@ -1,4 +1,5 @@
 /* testsub SUPPORT */
+/* charset=ISO8859-1 */
 /* lang=C++20 */
 
 /* revision history:
@@ -8,7 +9,7 @@
 
 */
 
-/* Copyright © 2017 David A­D­ Morano.  All rights reserved. */
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 /* Use is subject to license terms. */
 
 #include	<envstandards.h>	/* ordered first to configure */
