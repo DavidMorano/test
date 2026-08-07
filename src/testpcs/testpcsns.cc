b6500acebdb57ecf4a4bc5a9c2ed04efc88ebd61
David A.D. Morano <david.a.morano@gmail.com>
2026-08-07 00:50:27 -0400

    update

--- src/testpcs/testpcsns.cc
+++ src/testpcs/testpcsns.cc
@@ -14,7 +14,7 @@
 
 /* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
 
-#include	<envstandards.h>
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<sys/types.h>
 #include	<sys/param.h>
 #include	<usystem.h>
