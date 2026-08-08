c8730262c07d77685a2b8b6da5abf1a4f758995a
David A.D. Morano <david.a.morano@gmail.com>
2026-08-08 00:49:27 -0400

    cleanup

--- src/testput/main.cc
+++ src/testput/main.cc
@@ -23,7 +23,7 @@
 *************************************************************************/
 
 
-#include	<envstandards.h>
+#include	<envstandards.h>	/* ordered first to configure */
 
 #include	<sys/types.h>
 #include	<sys/stat.h>
