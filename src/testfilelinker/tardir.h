82375138760193946519ed4584440a83c43552eb
David A.D. Morano <david.a.morano@gmail.com>
2026-08-08 00:45:52 -0400

    update

--- src/testfilelinker/tardir.h
+++ src/testfilelinker/tardir.h
@@ -20,11 +20,11 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
 
 
 #endif /* TARDIR_INCLUDE */
