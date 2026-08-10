fa5398d565af5a10e6a9e2af6da32a2b53396993
David A.D. Morano <david.a.morano@gmail.com>
2026-08-10 00:49:54 -0400

    update

--- src/teststrnul/strnul.cc
+++ src/teststrnul/strnul.cc
@@ -29,16 +29,16 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<new>			/* |nothrow(3c++)| */
-#include	<string>
-#include	<usyscalls.h>
-#include	<ulogerror.h>
-#include	<localmisc.h>
-#include	<dprint.hh>		/* LIBU |DPRINT(3u)| */
-
-#include	"strnul.hh"
+#include	<cstddef>		/* CSTD |nullptr_t| */
+#include	<cstdlib>		/* CSTD */
+#include	<new>			/* C++STD |nothrow(3c++)| */
+#include	<string>		/* C++STD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
+#include	<ulogerror.h>		/* LIBU */
+#include	<strnul.hh>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
+#include	<dprint.hh>		/* LIBU |DPRINTF(3u)| */
 
 #pragma		GCC dependency		"mod/libutil.ccm"
 
