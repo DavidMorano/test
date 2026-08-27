c283d964f0f80e643be88718ac62be8295024aee
David A.D. Morano <david.a.morano@gmail.com>
2026-08-27 18:54:48 -0400

    update

--- src/testbio/main.cc
+++ src/testbio/main.cc
@@ -28,7 +28,7 @@
 *******************************************************************************/
 
 
-#include	<envstandards.h>
+#include	<envstandards.h>	/* ordered first to configure */
 
 #include	<sys/types.h>
 #include	<sys/param.h>
