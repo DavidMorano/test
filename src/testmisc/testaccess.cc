92894826781020fdd73f5a373fbc14243ed180ec
David A.D. Morano <david.a.morano@gmail.com>
2026-08-27 18:59:47 -0400

    restore

---
+++ src/testmisc/testaccess.cc
@@ -0,0 +1,38 @@
+/* testaccess */
+/* lang=C99 */
+
+
+/* revision history:
+
+	= 2000-05-14, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<sys/types.h>
+#include	<climits>
+#include	<unistd.h>
+#include	<cstdio>
+#include	<usystem.h>
+#include	<localmisc.h>
+#include	<exitcodes.h>
+
+
+/* ARGSUSED */
+int main(int argc,const char **argv,const char **envv)
+{
+	const int	am = R_OK ;
+	int		rs ;
+
+	rs = u_access("main.c",am) ;
+
+	printf("hooray! rs=%d\n",rs) ;
+
+	return EX_OK ;
+}
+/* end subroutine (main) */
+
+
