7b23d98ed821090869c726c01346c64d8645d893
David A.D. Morano <david.a.morano@gmail.com>
2026-08-27 19:05:20 -0400

    restore

---
+++ src/testtimezone/uctime.h
@@ -0,0 +1,35 @@
+/* uctime HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* time related facilities */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-05, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	UCTIME_INCLUDE
+#define	UCTIME_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+#include	<uctim.h>
+#include	<uctimeconv.h>
+#include	<uctimeout.h>
+#include	<uctimer.h>
+#include	<ucstrftime.h>
+
+
+#endif /* UCTIME_INCLUDE */
+
+
