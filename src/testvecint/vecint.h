2ed01cf15f6a5c8e7db7f238f4850e9c09e8a178
David A.D. Morano <david.a.morano@gmail.com>
2026-08-28 00:03:58 -0400

    update

--- src/testvecint/vecint.h
+++ src/testvecint/vecint.h
@@ -20,8 +20,8 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 
 #define	VECINT			struct vecint_head
