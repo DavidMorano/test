4f1c3db3faae4bf88193c4ef4c927870ce92632d
David A.D. Morano <david.a.morano@gmail.com>
2026-08-28 00:02:33 -0400

    update

--- src/testutmpacc/openx.h
+++ src/testutmpacc/openx.h
@@ -21,8 +21,8 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 #include	<opentmp.h>
 
