8ca05d2881cc95894fb6d7c532b37bb824f8e11a
David A.D. Morano <david.a.morano@gmail.com>
2026-08-28 00:04:53 -0400

    update

--- src/testvmail/testuterm.cc
+++ src/testvmail/testuterm.cc
@@ -24,7 +24,7 @@
 ******************************************************************************/
 
 
-#include	<envstandards.h>
+#include	<envstandards.h>	/* ordered first to configure */
 
 #include	<sys/types.h>
 #include	<cstdlib>
