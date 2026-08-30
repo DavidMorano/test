053608aff9877502fa030dc2f09f38bdd62d3209
David A.D. Morano <david.a.morano@gmail.com>
2026-08-30 00:14:12 -0400

    update

--- src/testtimestr/testtimestr_main.cc
+++ src/testtimestr/testtimestr_main.cc
@@ -5,8 +5,7 @@
 /* test the TIMESTR famility */
 /* version %I% last-modified %G% */
 
-#define	CF_DEBUG	1
-
+#define	CF_DEBUG	1		/* debugging */
 
 /* revision history:
 
@@ -19,31 +18,31 @@
 /* Use is subject to license terms. */
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<sys/types.h>
-#include	<sys/param.h>
-#include	<sys/stat.h>
-#include	<unistd.h>
-#include	<fcntl.h>
-#include	<ctime>
-#include	<csignal>
-#include	<cstddef>
-#include	<cstdlib>
-#include	<cstdio>
-#include	<cstring>
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<bfile.h>
-#include	<field.h>
-#include	<logfile.h>
-#include	<baops.h>
-#include	<timestr.h>
-#include	<localmisc.h>		/* |TIMEBUFLEN| */
+#include	<sys/types.h>		/* POSIX® */
+#include	<sys/param.h>		/* POSIX® */
+#include	<sys/stat.h>		/* POSIX® */
+#include	<unistd.h>		/* POSIX® */
+#include	<fcntl.h>		/* POSIX® */
+#include	<ctime>			/* CSTD */
+#include	<csignal>		/* CSTD */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD |tzset(3c)| */
+#include	<cstdio>		/* CSTD */
+#include	<cstring>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<baops.h>		/* LIBU */
+#include	<field.h>		/* LIBUC */
+#include	<logfile.h>		/* LIBUC */
+#include	<timestr.h>		/* LIBUC */
+#include	<localmisc.h>		/* LIBU |TIMEBUFLEN| */
+#include	<bfile.h>		/* LIBB */
 
 
 /* local defines */
 
-#ifndef	LINELEN
-#define	LINELEN		100
+#ifndef	CF_DEBUG
+#define	CF_DEBUG	1		/* debugging */
 #endif
 
 
@@ -55,17 +54,15 @@
 
 /* exported subroutines */
 
-int main(int,con mainv,con mainv) {)
-	char	linebuf[LINELEN + 1] ;
-	char	timebuf[TIMEBUFLEN + 1] ;
-	print("main: entered\n\n") ;
-	printf("tzname 1 %s %s\n",tzname[0],tzname[1]) ;
+int main(int,con mainv,con mainv) {
+    	custime		dt = time(nullptr) ;
+	char		timebuf[TIMEBUFLEN + 1] ;
+	printf("tzname 1 » %s %s «\n",tzname[0],tzname[1]) ;
+	printf("edate » %s «\n",timestr_edate(dt,timebuf)) ;
+	printf("tzname 2 » %s %s «\n",tzname[0],tzname[1]) ;
 	tzset() ;
-
-	printf("tzname 2 %s %s\n",tzname[0],tzname[1]) ;
-	printf("edate\n%s\n",timestr_edate(daytime,timebuf)) ;
-	printf("tzname 3 %s %s\n",tzname[0],tzname[1]) ;
-	printf("tzset edate\n%s\n",timestr_edate(daytime,timebuf)) ;
+	printf("tzname 3 » %s %s «\n",tzname[0],tzname[1]) ;
+	printf("edate » %s «\n",timestr_edate(dt,timebuf)) ;
 } /* end subroutine (main) */
 
 
