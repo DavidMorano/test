aba0afb7abec2e8c9fe3aef5d81b905fd195b593
David A.D. Morano <david.a.morano@gmail.com>
2026-09-03 00:43:52 -0400

    update

--- src/testmod/testmod_config.h
+++ src/testmod/testmod_config.h
@@ -2,7 +2,7 @@
 /* charset=ISO8859-1 */
 /* lang=C++20 */
 
-/* test modules */
+/* test module */
 /* version %I% last-modified %G% */
 
 
@@ -22,8 +22,20 @@
 
 /* Copyright © 2013 David A­D­ Morano.  All rights reserved. */
 
+#ifndef	TESTMODCONFIG_INCLUDE
+#define	TESTMODCONFIG_INCLUDE
+
+
 #include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
 
 #include	"testmod_sub.hh"
+
+
+#endif /* TESTMODCONFIG_INCLUDE */
 
 
