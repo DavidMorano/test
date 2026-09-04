d99edba472051ec96c61bb540b4aac555eb9d5ba
David A.D. Morano <david.a.morano@gmail.com>
2026-09-04 00:13:46 -0400

    update

--- src/testmisc/testlogsys.cc
+++ src/testmisc/testlogsys.cc
@@ -1,24 +1,49 @@
-/* testlogsys */
+/* testlogsys SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
+/* test program */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-13, David A-D- Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
+
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<sys/types.h>
 #include	<sys/syslog.h>
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
 #include	<cstdio>
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"logsys.h"
 
 
-int main()
-{
+int main() {
 	LOGSYS		logger, *lp = &logger ;
-	const int	fac = LOG_USER ;
-	const int	pri = LOG_INFO ;
+	cint	fac = LOG_USER ;
+	cint	pri = LOG_INFO ;
 	int		rs = 0 ;
 	int		rs1 ;
 	int		opts = 0 ;
-	const char	*logtab = "testlogsys" ;
+	cchar	*logtab = "testlogsys" ;
 
 	if ((rs = logsys_open(lp,fac,logtab,NULL,opts)) >= 0) {
-	    const char	*cp = "hello from the underworld!" ;
+	    cchar	*cp = "hello from the underworld!" ;
 
 	    rs = logsys_printf(lp,pri,cp) ;
 
@@ -27,7 +52,6 @@ int main()
 	} /* end if (logsys) */
 
 	return 0 ;
-}
-/* end subroutine (main) */
+} /* end subroutine (main) */
 
 
