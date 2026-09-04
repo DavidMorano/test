195a64d446a063309b54d5b8a3607aea905c5e6d
David A.D. Morano <david.a.morano@gmail.com>
2026-09-04 00:13:21 -0400

    update

--- src/testmisc/testmemfunc.cc
+++ src/testmisc/testmemfunc.cc
@@ -1,7 +1,33 @@
 /* testmemfunc SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
-#include	<iostream>
+/* test program */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-13, David A-D- Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
 #include	<cstdio>
+#include	<iostream>
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
 
 namespace {
     struct member ;
@@ -18,12 +44,11 @@ namespace {
 	    m = &member::printer ;
 	} ;
     } ; /* end struct (member) */
-}
+} /* end */
 
 int main() {
 	member	mem ;
 	mem.co() ;
-}
-/* end subroutine (main) */
+} /* end subroutine (main) */
 
 
