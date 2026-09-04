04145c00e3a4fc80f70940c3775e559809021350
David A.D. Morano <david.a.morano@gmail.com>
2026-09-04 00:13:49 -0400

    update

--- src/testmisc/testlookup.cc
+++ src/testmisc/testlookup.cc
@@ -1,8 +1,24 @@
-/* testinit (C89) */
+/* testlookup SUPPOET */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
-#define	CF_DEBUGS	1		/* non-switchable debug print-outs */
+/* test program */
+/* version %I% last-modified %G% */
+
+#define	CF_DEBUG	1		/* non-switchable debug print-outs */
 #define	CF_DEBUG	1		/* switchable at invocation */
 #define	CF_DEBUGMALL	1		/* debug memory allocation */
+
+
+/* revision history:
+
+	= 1998-04-13, David A-D- Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
 
 #include	<envstandards.h>	/* ordered first to configure */
 #include	<sys/types.h>
@@ -16,28 +32,28 @@
 #define	VARDEBUGFNAME	"TESTGETHOST_DEBUGFILE"
 #endif
 
-#if	CF_DEBUGS || CF_DEBUG
-extern int	debugopen(const char *) ;
-extern int	debugprintf(const char *,...) ;
+#if	CF_DEBUG || CF_DEBUG
+extern int	debugopen(cchar *) ;
+extern int	debugprintf(cchar *,...) ;
 extern int	debugclose() ;
-extern int	strlinelen(const char *,int,int) ;
+extern int	strlinelen(cchar *,int,int) ;
 #endif
 
 
-int main(int argc,const char **argv,const char **envv) {
+int main(int argc,cchar **argv,cchar **envv) {
 	int		rs = SR_OK ;
-	const char	*cp ;
+	cchar	*cp ;
 
-#if	CF_DEBUGS || CF_DEBUG
+#if	CF_DEBUG || CF_DEBUG
 	if ((cp = getourenv(envv,VARDEBUGFNAME)) != NULL) {
 	    rs = debugopen(cp) ;
 	    debugprintf("main: starting DFD=%d\n",rs) ;
 	}
-#endif /* CF_DEBUGS */
+#endif /* CF_DEBUG */
 
 	if (argc > 0) {
 	    HOSTENT	he ;
-	    const int	helen = bufsizeget(bufsizeget_he) ;
+	    cint	helen = bufsizeget(bufsizeget_he) ;
 	    char	*hebuf ;
 	    if ((rs = uc_malloc((helen+1),&hebuf)) >= 0) {
 	    	int	ai ;
@@ -52,11 +68,10 @@ int main(int argc,const char **argv,const char **envv) {
 	    } /* end if (memory-allocation) */
 	} /* end if (positive) */
 
-#if	(CF_DEBUGS || CF_DEBUG)
+#if	(CF_DEBUG || CF_DEBUG)
 	debugclose() ;
 #endif
 
 	return 0 ;
-}
-/* end subroutine (main) */
+} /* end subroutine (main) */
 
