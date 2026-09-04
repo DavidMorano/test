8fd1804a9a098eb92f42120e42185ddb66eeba05
David A.D. Morano <david.a.morano@gmail.com>
2026-09-04 00:10:22 -0400

    update

--- src/testmodule/testmod.hh
+++ src/testmodule/testmod.hh
@@ -6,8 +6,27 @@
 /* version %I% last-modified %G% */
 
 
+/* revision history:
+
+	= 2020-05-04, David A-D- Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2020 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
+
+#ifndef	TESTMOD_INCLUDE
+#define	TESTMOD_INCLUDE
+#ifdef	__cplusplus
+
+
 #ifndef	NAME
 #define	NAME	"David Morano"
 #endif
+
+
+#endif /* __cplusplus */
+#endif /* TESTMOD_INCLUDE */
 
 
