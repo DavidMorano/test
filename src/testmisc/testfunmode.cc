53be49bed20a29ec1087d734b2781e48e52fa489
David A.D. Morano <david.a.morano@gmail.com>
2026-09-04 00:12:34 -0400

    update

--- src/testmisc/testfunmode.cc
+++ src/testmisc/testfunmode.cc
@@ -2,6 +2,20 @@
 /* charset=ISO8859-1 */
 /* lang=C++98 */
 
+/* test program */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-13, David A-D- Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
+
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<cstddef>		/* CSTD */
 #include	<cstdlib>		/* CSTD */
@@ -16,9 +30,11 @@ using std::cout ;			/* variable */
 
 int main(int argc,con mainv argv,con mainv envv) {
     	funmode	fc{} ;
+	(void) argc ;
+	(void) argv ;
+	(void) envv ;
 	cout << "fc=" << int(fc) << eol ;
 	cout << "funmode::rd=" << int(funmode::rd) << eol ;
-
 } /* end subroutine (main) */
 
 
