c62172b05ea1e9341263e6e854c185d55bb4f051
David A.D. Morano <david.a.morano@gmail.com>
2026-09-04 00:08:45 -0400

    restore

---
+++ src/testmod/modhello.hh
@@ -0,0 +1,64 @@
+/* modhello HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* test modules */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2020-05-11, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2020 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
+
+/*******************************************************************************
+
+  	Name:
+	modhello
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
+
+
+/* local defines */
+
+
+/* local namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
