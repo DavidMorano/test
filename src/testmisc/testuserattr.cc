064dba6c2a23da941f677053d3b5444d34518082
David A.D. Morano <david.a.morano@gmail.com>
2026-09-04 00:16:33 -0400

    update

--- src/testmisc/testuserattr.cc
+++ src/testmisc/testuserattr.cc
@@ -1,26 +1,43 @@
-/* testuserattr (C89) */
+/* testuserattr SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
+
+/* test program */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-13, David A-D- Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
 
 #include	<envstandards.h>	/* ordered first to configure */
-
 #include	<sys/types.h>
 #include	<user_attr.h>
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
 #include	<cstdio>
-
-#include	<usystem.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 #include	<localmisc.h>
 
-int main()
-{
+int main() {
 	userattr_t	*uap ;
 
 	int	rs ;
 
-	const char	*un = "junker" ;
+	cchar	*un = "junker" ;
 
 	rs = uc_getusernam(un,&uap) ;
 
 	fprintf(stderr,"main: uc_getusernam() rs=%d\n",rs) ;
 
 	return 0 ;
-}
+} /* end subroutine (main) */
+
 
