bffb7c01fbb3416b230d5c42715ce0898e61334f
David A.D. Morano <david.a.morano@gmail.com>
2026-09-04 00:11:21 -0400

    update

--- src/testmisc/testattach.cc
+++ src/testmisc/testattach.cc
@@ -1,17 +1,30 @@
-/* main (testattach) */
+/* testattach SUPPORT (testattach) */
+/* charset=ISO8859-1 */
+/* lang=C99 */
 
+/* test program */
+/* version %I% last-modified %G% */
 
-#define	CF_DEBUGS	1		/* compile-time debugging */
+#define	CF_DEBUG	1		/* compile-time debugging */
 #define	CF_DEBUGMALL	1		/* debug memory-allocations */
 
+/* revision history:
+
+	= 1998-04-13, David A-D- Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
 
 #include	<envstandards.h>	/* ordered first to configure */
-
 #include	<sys/types.h>
 #include	<climits>
-#include	<cstdlib>
-
-#include	<usystem.h>
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 #include	<localmisc.h>
 
 #include	"defs.h"
@@ -22,39 +35,33 @@
 #define	MKCHAR(ch)	((ch) & UCHAR_MAX)
 #endif
 
-extern int	sfcookkey(const char *,int,const char **) ;
+extern int	sfcookkey(cchar *,int,cchar **) ;
 
-#if	CF_DEBUGS
-extern int	debugopen(const char *) ;
-extern int	debugprintf(const char *,...) ;
-extern int	debugprinthex(const char *,int,const char *,int) ;
+#if	CF_DEBUG
+extern int	debugopen(cchar *) ;
+extern int	debugprintf(cchar *,...) ;
+extern int	debugprinthex(cchar *,int,cchar *,int) ;
 extern int	debugclose() ;
-extern int	strlinelen(const char *,int,int) ;
+extern int	strlinelen(cchar *,int,int) ;
 #endif
 
 
-int main(argc,argv,envv)
-int		argc ;
-const char	*argv[] ;
-const char	*envv[] ;
-{
-	const int	klen = KBUFLEN ;
-	const int	wch = MKCHAR('¿') ;
-
+int main(int argc,con mainv argv,con mainv envv) {
+	cint	klen = KBUFLEN ;
+	cint	wch = MKCHAR('¿') ;
 	uint	mo_start = 0 ;
-
 	int	rs ;
 	int	cl ;
-
-	const char	*s = "here %R is %{tz} thing %{} %{junker} end" ;
-	const char	*cp ;
-	const char	*ad = "ad" ;
-	const char	*mnt = "mnt" ;
-
+	cchar	*s = "here %R is %{tz} thing %{} %{junker} end" ;
+	cchar	*cp ;
+	cchar	*ad = "ad" ;
+	cchar	*mnt = "mnt" ;
 	char	kbuf[KBUFLEN+1] = { 0 } ;
 
-
-#if	CF_DEBUGS
+	(void) argc ;
+	(void) argv ;
+	(void) envv ;
+#if	CF_DEBUG
 	if ((cp = getenv(VARDEBUGFNAME)) == NULL) {
 	    if ((cp = getenv(VARDEBUGFD1)) == NULL)
 	        cp = getenv(VARDEBUGFD2) ;
@@ -62,9 +69,9 @@ const char	*envv[] ;
 	if (cp != NULL)
 	    debugopen(cp) ;
 	debugprintf("main: starting\n") ;
-#endif /* CF_DEBUGS */
+#endif /* CF_DEBUG */
 
-#if	CF_DEBUGS && CF_DEBUGMALL
+#if	CF_DEBUG && CF_DEBUGMALL
 	uc_mallset(1) ;
 	uc_mallout(&mo_start) ;
 #endif
@@ -72,30 +79,30 @@ const char	*envv[] ;
 	if ((rs = u_open(ad,O_RDONLY,0666)) >= 0) {
 	    int	dfd = rs ;
 
-#if	CF_DEBUGS
+#if	CF_DEBUG
 	    debugprintf("main: u_open() rs=%d\n",rs) ;
-#endif /* CF_DEBUGS */
+#endif /* CF_DEBUG */
 
 	    if ((rs = uc_fattach(dfd,mnt)) >= 0) {
 
-#if	CF_DEBUGS
+#if	CF_DEBUG
 	    debugprintf("main: uc_fattach() rs=%d\n",rs) ;
-#endif /* CF_DEBUGS */
+#endif /* CF_DEBUG */
 		sleep(60) ;
 	    }
 
-#if	CF_DEBUGS
+#if	CF_DEBUG
 	    debugprintf("main: uc_fattach rs=%d\n",rs) ;
-#endif /* CF_DEBUGS */
+#endif /* CF_DEBUG */
 
 	    u_close(dfd) ;
 	} /* end if (open) */
 
-#if	CF_DEBUGS
+#if	CF_DEBUG
 	    debugprintf("main: exiting rs=%d\n",rs) ;
-#endif /* CF_DEBUGS */
+#endif /* CF_DEBUG */
 
-#if	CF_DEBUGS && CF_DEBUGMALL
+#if	CF_DEBUG && CF_DEBUGMALL
 	{
 	    uint	mo ;
 	    uc_mallout(&mo) ;
@@ -104,12 +111,11 @@ const char	*envv[] ;
 	}
 #endif
 
-#if	CF_DEBUGS
+#if	CF_DEBUG
 	debugclose() ;
 #endif
 
 	return 0 ;
-}
-/* end subroutine (main) */
+} /* end subroutine (main) */
 
 
