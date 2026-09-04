14382a0b3f9622de9382c8388c3f7abbbf6d14f7
David A.D. Morano <david.a.morano@gmail.com>
2026-09-04 00:12:23 -0400

    update

--- src/testmisc/testhardvard.cc
+++ src/testmisc/testhardvard.cc
@@ -1,12 +1,39 @@
 /* main SUPPORT (test-harvard) */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
 
+/* test program */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-13, David A-D- Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
 #include	<cstdio>
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
+
 
 typedef unsigned int	uint ;
 
 int main() {
     uint	a = (16 << 20) - (16 << 16) ;
     printf("%08x %d\n",a,a) ;
-}
+} /* end */
 
 
