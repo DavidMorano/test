bf5c5ad4e2909331208fd093d96fda6b1bf6dbb4
David A.D. Morano <david.a.morano@gmail.com>
2026-09-05 00:50:26 -0400

    update

--- src/testmisc/testscope.cc
+++ src/testmisc/testscope.cc
@@ -1,10 +1,36 @@
-/* testscope */
+/* testscope SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
+/* test program */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-13, David A-D- Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
 #include	<cstdio>
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
 
-static int	a ;
+local int	a ;
 
-static void foo(void) ;
+local void foo(void) ;
 
 int main()
 {
@@ -13,8 +39,8 @@ int main()
 	return 0 ;
 }
 
-static void foo(void) {
-	static int	a = 0 ;
+local void foo(void) {
+	local int	a = 0 ;
 	a += 1 ;
 	printf("a=%u\n",a) ;
 }
