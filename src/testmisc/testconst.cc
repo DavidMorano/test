11194b66aa0f209bae819b02ce8a0b930d292d6a
David A.D. Morano <david.a.morano@gmail.com>
2026-09-05 00:49:06 -0400

    update

--- src/testmisc/testconst.cc
+++ src/testmisc/testconst.cc
@@ -1,9 +1,28 @@
-/* main (testconst) */
+/* testconst SUPPORT (testconst) */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* test program */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-13, David A-D- Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
 
 #include	<envstandards.h>	/* ordered first to configure */
 #include	<sys/types.h>
-#include	<cstddef>
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
 #include	<cstdio>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 #include	<localmisc.h>
 
 #if	defined(_WCHAR_T)
@@ -12,18 +31,15 @@
 #define	WCHAR	0
 #endif
 
-typedef int	xchar_t ;
 
-int main(int argc,cchar **argv,cchar **envv)
-{
-	const int64_t	a = 0 ;
-	const xchar_t	j = 1 ;
-	wchar_t		b ;
-
+int main(int argc,con mainv argv,con mainv *envv) {
+	cint64_t	a = 0 ;
+	(void) argc ;
+	(void) argv ;
+	(void) envv ;
 	printf("%lld wchar=%u\n",a,WCHAR) ;
 
 	return 0 ;
-}
-/* end subroutine (main) */
+} /* end subroutine (main) */
 
 
