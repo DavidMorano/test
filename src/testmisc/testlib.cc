5c02870a9fdae829bdb66bee2caa7f1f8a2d48f2
David A.D. Morano <david.a.morano@gmail.com>
2026-09-05 00:49:32 -0400

    update

--- src/testmisc/testlib.cc
+++ src/testmisc/testlib.cc
@@ -1,14 +1,40 @@
+/* testlin SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
+/* test program */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-13, David A-D- Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
 #include	<cstdio>
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
 
 extern void	sub1(int) ;
 extern void	sub2(int) ;
 
-int main()
-{
+int main() {
 	sub2(2) ;
 	sub1(1) ;
 	return 0 ;
-}
+} /* end */
 
 
