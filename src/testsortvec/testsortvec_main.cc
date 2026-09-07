68c0a45f2be1ed85345ddd1c31ca294484cda238
David A.D. Morano <david.a.morano@gmail.com>
2026-09-07 00:11:33 -0400

    update

--- src/testsortvec/testsortvec_main.cc
+++ src/testsortvec/testsortvec_main.cc
@@ -236,11 +236,10 @@ badopen:
 
 	goto done ;
 
-}
-/* end subroutine (main) */
+} /* end subroutine (main) */
 
 local int slcmp(cchar **app,cchar **bpp) noex {
 	return strcmp(*app,*bpp) ;
-}
+} /* end */
 
 
