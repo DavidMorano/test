7d76756e4feb4e3627860f412c0e54f6f0d43947
David A.D. Morano <david.a.morano@gmail.com>
2026-09-07 00:09:12 -0400

    update

--- src/testdstr/testdstr_main.cc
+++ src/testdstr/testdstr_main.cc
@@ -1,29 +1,26 @@
-/* main */
+/* main SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
 /* test the DSTR object */
+/* version %I% last-modified %G% */
 
 
+/* exported variables */
 
-int main()
-{
+
+/* exported subroutines */
+
+int main(int,con mainv,con mainv) {
 	DSTR	a ;
 	DSTR	b ;
-
-
 	dstr_start(&a,"here",-1) ;
-
 	dstr_start(&a,NULL,0) ;
-
 
 	dstr_assign(&b,&a) ;
 
-
 	dstr_finish(&a) ;
-
 	dstr_finish(&b) ;
-
-	return 0 ;
-}
-
+} /* end subroutine (main) */
 
 
