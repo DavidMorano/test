76cac6ba4c82e0ffd727f7d8ea7c1b510c749a93
David A.D. Morano <david.a.morano@gmail.com>
2026-09-07 00:10:23 -0400

    update

--- src/testbase64/testbase64_main.cc
+++ src/testbase64/testbase64_main.cc
@@ -168,8 +168,7 @@ int main(int argc,con mainv argv,con mainv) {
 	    ex = EXIT_FAILURE ;
 	}
 	return ex ;
-}
-/* end subroutine (main) */
+} /* end subroutine (main) */
 
 
 /* local subroutines */
