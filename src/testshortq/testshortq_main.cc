fc6f15a4ab56ec91cb268074de8adecfc7153d4c
David A.D. Morano <david.a.morano@gmail.com>
2026-09-07 00:11:18 -0400

    update

--- src/testshortq/testshortq_main.cc
+++ src/testshortq/testshortq_main.cc
@@ -156,8 +156,7 @@ int main(int argc,con mainv argv,con mainv) {
 	    ex = EXIT_FAILURE ;
 	}
 	return ex ;
-}
-/* end subroutine (main) */
+} /* end subroutine (main) */
 
 
 /* local subroutines */
