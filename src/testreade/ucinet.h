74a3953e26956afae5796ca2c400d9ab4fdb503d
David A.D. Morano <david.a.morano@gmail.com>
2026-09-08 00:50:47 -0400

    restore

---
+++ src/testreade/ucinet.h
@@ -0,0 +1,42 @@
+/* ucinet HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* UNIX® UCINET support */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-13, David A­D­ Morano
+	This module was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Group:
+	ucinet
+
+	Description:
+	This file contains some support for the UNIX® UCINET
+	related operations.
+
+*******************************************************************************/
+
+#ifndef	UCINET_INCLUDE
+#define	UCINET_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+#include	<ucinetconv.h>
+
+
+#endif /* UCINET_INCLUDE */
+
+
