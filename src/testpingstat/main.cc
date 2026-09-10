0abd6e4b6381cf39c732ee638a9f71b430ca5ed3
David A.D. Morano <david.a.morano@gmail.com>
2026-09-10 00:47:03 -0400

    update

--- src/testpingstat/main.cc
+++ src/testpingstat/main.cc
@@ -21,7 +21,7 @@
 
 *******************************************************************************/
 
-#include	<envstandards.h>
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<sys/types.h>
 #include	<sys/socket.h>
 #include	<sys/time.h>
