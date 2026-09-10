b7e40670b1cb9b54f2530498825b21cf0a8724e5
David A.D. Morano <david.a.morano@gmail.com>
2026-09-10 00:48:24 -0400

    update

--- src/testuss/main.cc
+++ src/testuss/main.cc
@@ -11,7 +11,7 @@
 /* Copyright © 2002 David A­D­ Morano.  All rights reserved. */
 
 
-#include	<envstandards.h>
+#include	<envstandards.h>	/* ordered first to configure */
 
 #include	<sys/types.h>
 #include	<usystem.h>
