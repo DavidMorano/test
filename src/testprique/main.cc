f35305f79a20655c6e161e69199e1924b2d3616a
David A.D. Morano <david.a.morano@gmail.com>
2026-09-10 00:47:26 -0400

    update

--- src/testprique/main.cc
+++ src/testprique/main.cc
@@ -1,4 +1,4 @@
-/* main SUPPORT (testprique) */
+/* testprique_main SUPPORT (testprique) */
 /* charset=ISO8859-1 */
 /* lang=C++20 */
 
@@ -13,7 +13,7 @@
 
 */
 
-/* Copyright © 2017 David A­D­ Morano.  All rights reserved. */
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 /* Use is subject to license terms. */
 
 #include	<envstandards.h>	/* ordered first to configure */
