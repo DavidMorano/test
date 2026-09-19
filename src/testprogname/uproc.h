cfa62baf2696f460bc62a0d0bd19becb2560f996
David A.D. Morano <david.a.morano@gmail.com>
2026-09-19 00:44:46 -0400

    restore

---
+++ src/testprogname/uproc.h
@@ -0,0 +1,45 @@
+/* uproc HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* UNIX® process mangement */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-21, David A­D­ Morano
+	This module was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Group:
+	uproc
+
+	Description:
+	This module provides UNIX® process mangement.
+
+*******************************************************************************/
+
+#ifndef	UPROC_INCLUDE
+#define	UPROC_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+#include	<uexec.h>
+#include	<uexecname.h>
+#include	<unanosleep.h>
+#include	<uprocess.h>
+#include	<usig.h>
+
+
+#endif /* UPROC_INCLUDE */
+
+
