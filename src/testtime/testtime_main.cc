597fc9444247d82a596ff3e89b98d9ee7fcda4d7
David A.D. Morano <david.a.morano@gmail.com>
2026-09-20 00:36:50 -0400

    update

--- src/testtime/testtime_main.cc
+++ src/testtime/testtime_main.cc
@@ -5,32 +5,43 @@
 /* test time */
 /* version %I% last-modified %G% */
 
+
+/* revision history:
+
+	= 1998-04-13, David A-D- Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
+
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<sys/types.h>
-#include	<sys/param.h>
-#include	<sys/stat.h>
-#include	<sys/timeb.h>
-#include	<ctime>
-#include	<climits>		/* |INT_MAX| */
-#include	<cstddef>
-#include	<cstdlib>
-#include	<cstdio>
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<timestr.h>
-#include	<localmisc.h>		/* |TIMEBUFLEN| */
+#include	<sys/types.h>		/* POSIX® */
+#include	<sys/param.h>		/* POSIX® */
+#include	<sys/stat.h>		/* POSIX® */
+#include	<sys/timeb.h>		/* POSIX® */
+#include	<ctime>			/* CSTD */
+#include	<climits>		/* CSTD |INT_MAX| */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<cstdio>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<timestr.h>		/* LIBUC */
+#include	<localmisc.h>		/* LIBU |TIMEBUFLEN| */
 
 extern "C" {
     extern int uc_ftime(TIMEB *) noex ;
 } /* end */
 
 int main(int,con mainv,con mainv) {
-	time_t	daytime = time(nullptr) ;
+	time_t	dt = time(nullptr) ;
 	int	rs = SR_OK ;
 	char	timebuf[TIMEBUFLEN + 1] ;
 	if (rs >= 0) {
 	    time_t	a[6] = {} ;
-	    a[0] = daytime ;
+	    a[0] = dt ;
 	    a[1] = INT_MAX ;
 	    for (int i = 0 ; (rs >= 0) && (i < 2) ; i += 1) {
 	        printf("time=\\x%08lx (%s)\n",
