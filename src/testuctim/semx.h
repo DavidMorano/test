51efa821c32651cf936ecc56c33fe0bd46240071
David A.D. Morano <david.a.morano@gmail.com>
2026-09-23 00:01:33 -0400

    restore

---
+++ src/testuctim/semx.h
@@ -0,0 +1,33 @@
+/* semx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* string-index operations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	SEMX_INCLUDE
+#define	SEMX_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+#include	<csem.h>
+#include	<psem.h>
+#include	<namesem.h>
+
+
+#endif /* SEMX_INCLUDE */
+
+
