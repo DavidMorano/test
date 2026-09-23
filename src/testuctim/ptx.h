fa1fc7c0dd00db9d96044ca74d8295d93bb79618
David A.D. Morano <david.a.morano@gmail.com>
2026-09-23 00:01:26 -0400

    restore

---
+++ src/testuctim/ptx.h
@@ -0,0 +1,43 @@
+/* ptx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* version %I% last-modified %G% */
+/* UNIX® system-calls */
+
+
+/* revision history:
+
+	= 1998-03-21, David A­D­ Morano
+	This module was originally written.
+
+	= 2017-08-01, David A­D­ Morano
+	Updated for lack of interfaces in MacOS Darwin
+
+*/
+
+/* Copyright © 1998,2017 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	USYSCALLS_INCLUDE
+#define	USYSCALLS_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<pthread.h>		/* POSIX® */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+#include	<pta.h>
+#include	<ptc.h>
+#include	<ptca.h>
+#include	<ptm.h>
+#include	<ptma.h>
+#include	<ptrwa.h>
+#include	<ptrwlock.h>
+
+#include	<lockrw.h>
+
+
+#endif /* USYSCALLS_INCLUDE */
+
+
