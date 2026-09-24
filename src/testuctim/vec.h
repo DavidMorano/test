6175e462242778abffdd4270323078afe18d5b6a
David A.D. Morano <david.a.morano@gmail.com>
2026-09-24 00:05:26 -0400

    restore

---
+++ src/testuctim/vec.h
@@ -0,0 +1,42 @@
+/* vec HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* vector operatins */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	VEC_INCLUDE
+#define	VEC_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+#include	<raqhand.h>
+#include	<recarr.h>
+#include	<vecelem.h>
+#include	<vechand.h>
+#include	<vecitem.h>
+#include	<vecobj.h>
+#include	<vecpstr.h>
+#include	<vecsorthand.h>
+#include	<vecstr.h>
+#include	<vecint.h>
+#include	<veclong.h>
+#include	<vsetstr.h>
+
+
+#endif /* VEC_INCLUDE */
+
+
